535a2a0ecfb7f0ca1fc552845d5c0afcdc30734b
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2012-10-26 20:06:23 +0000

    * Import grids.esmp only if the ESMP is installed.

--- cmt/grids/__init__.py
+++ cmt/grids/__init__.py
@@ -5,5 +5,13 @@ from structured import *
 from unstructured import *
 from field import *
 from igrid import *
-from esmp import *
-from map import *
+try:
+    from esmp import *
+except ImportError:
+    from warnings import warn
+    warn ('ESMF mapper disabled', RuntimeWarning)
+try:
+    from map import *
+except (ImportError, OSError):
+    from warnings import warn
+    warn ('CSDMS mapper disabled', RuntimeWarning)
