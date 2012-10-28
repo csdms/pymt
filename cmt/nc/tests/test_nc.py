14b31c3dd10b2526630da6d026236f1fc4e54ddf
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2012-10-28 21:14:15 +0000

    * Test contents of NetCDF files

--- cmt/nc/tests/test_nc.py
+++ cmt/nc/tests/test_nc.py
@@ -4,7 +4,7 @@ import os
 import unittest
 import numpy as np
 
-from cmt.grids import RasterField
+from cmt.grids import RasterField, UnstructuredField
 from cmt.nc import field_tofile
 
 class TestNC (unittest.TestCase):
@@ -13,6 +13,7 @@ class TestNC (unittest.TestCase):
         test_1d='test-1d-00.nc',
         test_2d='test-1d-00.nc',
         test_3d='test-1d-00.nc',
+        test_2d_unstructured='test-2d-00.ncu',
     )
 
     def setUp (self):
@@ -29,6 +30,31 @@ class TestNC (unittest.TestCase):
             except OSError:
                 pass
 
+    def assert_dimension_keys (self, root, expected_keys):
+        keys = root.dimensions.keys ()
+        self.assertItemsEqual (expected_keys, keys)
+
+    def assert_var_names (self, root, expected_names):
+        vars = root.variables
+        self.assertItemsEqual (vars.keys (), expected_names)
+    def assert_var_values (self, root, var, expected_values):
+        vars = root.variables
+        values = vars[var][:]
+        self.assertListEqual (list (values), list (expected_values))
+    def assert_var_name (self, root, var, expected_name):
+        vars = root.variables
+        name = vars[var].long_name
+        self.assertEqual (name, expected_name)
+    def assert_var_units (self, root, var, expected_units):
+        vars = root.variables
+        units = vars[var].units
+        self.assertEqual (units, expected_units)
+
+    @staticmethod
+    def open_nc_file (file):
+        import netCDF4 as nc
+        return nc.Dataset (file, 'r', format='NETCDF4')
+
     def test_2d (self):
         nc_file = self.nc_files['test_2d']
         field = RasterField ((3,2), (2.,1), (0,0.5),
@@ -41,15 +67,56 @@ class TestNC (unittest.TestCase):
         field.add_field ('Velocity', data*100, centering='point', units='m/s')
         field.add_field ('Temp', data*2, centering='point', units='F')
 
-        try:
-            os.remove (nc_file)
-        except OSError:
-            pass
-
         attrs = dict (description='Example nc file', author='Eric')
         field_tofile (field, nc_file, attrs=attrs, append=True)
 
         self.assertTrue (os.path.isfile (nc_file))
+
+        with self.open_nc_file (nc_file) as root:
+            self.assert_var_names (root, ['Temperature', 'Elevation',
+                                          'Velocity', 'Temp',
+                                          'x', 'y', 't', 'dummy'])
+            self.assert_dimension_keys (root, ['nx', 'ny', 'nt'])
+            self.assert_var_values (root, 'x', [.5, 1.5])
+            self.assert_var_values (root, 'y', [0., 2., 4.])
+
+            for name in ['Temperature', 'Elevation', 'Velocity', 'Temp']:
+                self.assert_var_name (root, name, name)
+            for (v, u) in dict (Temperature='C', Elevation='meters',
+                                Velocity='m/s', Temp='F').items ():
+                self.assert_var_units (root, v, u)
+
+    def test_2d_unstructured (self):
+        nc_file = self.nc_files['test_2d_unstructured']
+
+        field = UnstructuredField ([0, 2, 1, 3], [0, 0, 1, 1],
+                                   [0, 2, 1, 2, 3, 1], [3, 6],
+                                   units=('m', 'm'))
+
+        data = np.arange (4)
+        field.add_field ('point_field', data, centering='point', units='m')
+
+        data = np.arange (2)
+        field.add_field ('cell_field', data, centering='zonal',
+                         units='m^2')
+
+        field_tofile (field, nc_file, append=False)
+
+        with self.open_nc_file (nc_file) as root:
+            self.assert_var_names (root, ['point_field', 'cell_field',
+                                          'x', 'y', 't', 'connectivity',
+                                          'offset', 'dummy'])
+            self.assert_dimension_keys (root, ['n_cells', 'n_points',
+                                               'n_vertices', 'nt'])
+            self.assert_var_values (root, 'x', [0., 2., 1., 3.])
+            self.assert_var_values (root, 'y', [0., 0., 1., 1.])
+            self.assert_var_values (root, 'offset', [3, 6])
+            self.assert_var_values (root, 'connectivity', [0, 2, 1, 2, 3, 1])
+
+            self.assert_var_name (root, 'point_field', 'point_field')
+            self.assert_var_name (root, 'cell_field', 'cell_field')
+            self.assert_var_units (root, 'point_field', 'm')
+            self.assert_var_units (root, 'cell_field', 'm^2')
 
     def test_3d (self):
         nc_file = self.nc_files['test_3d']
@@ -63,11 +130,6 @@ class TestNC (unittest.TestCase):
         field.add_field ('Velocity', data*100, centering='point', units='m/s')
         field.add_field ('Temp', data*2, centering='point', units='F')
 
-        try:
-            os.remove (nc_file)
-        except OSError:
-            pass
-
         attrs = dict (description='Example 3D nc file', author='Eric')
         field_tofile (field, nc_file, attrs=attrs, append=True)
 
@@ -79,31 +141,41 @@ class TestNC (unittest.TestCase):
         data = np.arange (12.)
         field.add_field ('Elevation', data, centering='point')
 
-        try:
-            os.remove (nc_file)
-        except OSError:
-            pass
-
         attrs = dict (description='Example 1D nc file', author='Eric')
         field_tofile (field, nc_file, attrs=attrs, append=True)
 
         self.assertTrue (os.path.isfile (nc_file))
 
+        with self.open_nc_file (nc_file) as root:
+            self.assert_var_names (root, ['Elevation', 'x', 't', 'dummy'])
+            self.assert_dimension_keys (root, ['nx', 'nt'])
+            self.assert_var_values (root, 'x', np.arange (12.) - 1.)
+
+            self.assert_var_name (root, 'Elevation', 'Elevation')
+            self.assert_var_units (root, 'Elevation', '-')
+
+            self.assert_var_units (root, 'x', 'm')
+
     def test_0d (self):
         nc_file = self.nc_files['test_0d']
         field = RasterField ((1, ), (0, ), (0, ), indexing='ij', units=('m', ))
 
-        try:
-            os.remove (nc_file)
-        except OSError:
-            pass
-
         attrs = dict (description='Example 0D nc file', author='Eric')
         for i in range (10):
-            field.add_field ('Elevation', i, centering='point')
+            field.add_field ('Elevation', i*10., centering='point')
             field_tofile (field, nc_file, attrs=attrs, append=True)
 
         self.assertTrue (os.path.isfile (nc_file))
+
+        with self.open_nc_file (nc_file) as root:
+            self.assert_var_names (root, ['Elevation', 't', 'dummy'])
+            self.assert_dimension_keys (root, ['nt'])
+
+            self.assert_var_name (root, 'Elevation', 'Elevation')
+            self.assert_var_units (root, 'Elevation', '-')
+
+            self.assert_var_values (root, 'Elevation', 10.*np.arange (10))
+            self.assert_var_values (root, 't', range (10))
 
 if __name__ == '__main__':
     unittest.main ()
