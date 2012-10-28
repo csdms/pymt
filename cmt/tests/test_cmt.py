198b8b3a76187e18af6972bcfe5f32974681a384
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2012-10-28 21:39:07 +0000

    * Add tests form mapper and field
      Add tests for cmt.mappers.mapper and
      cmt.grids.field to cmt package tests.

--- cmt/tests/test_cmt.py
+++ cmt/tests/test_cmt.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 
 import unittest
-from unittest import defaultTestLoader as loader
+#from unittest import defaultTestLoader as loader
 
 from cmt.grids.tests.test_raster import *
 from cmt.grids.tests.test_rectilinear import *
+from cmt.grids.tests.test_field import *
 from cmt.nc.tests.test_nc import *
+from cmt.mappers.tests.test_mapper import *
 
 if __name__ == '__main__':
     unittest.main ()
