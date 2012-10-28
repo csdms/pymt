c46876b1b8392edabb5516a8ce1f30a595729158
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2012-10-28 19:16:41 +0000

    * Fix to run all tests

--- cmt/tests/test_cmt.py
+++ cmt/tests/test_cmt.py
@@ -3,22 +3,10 @@
 import unittest
 from unittest import defaultTestLoader as loader
 
-import cmt.grids.tests.test_raster
-import cmt.grids.tests.test_rectilinear
-import cmt.nc.tests.test_nc
-
-suite = unittest.TestSuite ()
-suite.addTests (
-    loader.loadTestsFromModule (cmt.grids.tests.test_raster))
-suite.addTests (
-    loader.loadTestsFromModule (cmt.grids.tests.test_rectilinear))
-suite.addTests (
-    loader.loadTestsFromModule (cmt.nc.tests.test_nc))
-
-unittest.TextTestRunner (verbosity=2).run (suite)
+from cmt.grids.tests.test_raster import *
+from cmt.grids.tests.test_rectilinear import *
+from cmt.nc.tests.test_nc import *
 
 if __name__ == '__main__':
-    #unittest.main ()
-
-    unittest.TextTestRunner (verbosity=2).run (suite)
+    unittest.main ()
 
