c88ccfbdf98b579e5716777a2db77bad92f264c4
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2013-10-29 22:12:01 +0000

    * Changed for style.

--- cmt/bov/bov.py
+++ cmt/bov/bov.py
@@ -6,48 +6,66 @@ import ConfigParser
 
 import numpy as np
 
-#from cmt import VTKGridUniformRectilinear, VTKGridPoints
 from cmt.grids import RasterField
 
-class BovError (Exception):
+
+class BovError(Exception):
     pass
-class MissingRequiredKeyError (BovError):
-    def __init__ (self, opt):
+
+
+class MissingRequiredKeyError(BovError):
+    def __init__(self, opt):
         self.opt = opt
-    def __str__ (self):
+
+    def __str__(self):
         return '%s: Missing required key' % self.opt
-class BadKeyValueError (BovError):
-    def __init__ (self, key, value):
+
+
+class BadKeyValueError(BovError):
+    def __init__(self, key, value):
         self.key = key
         self.value = value
-    def __str__ (self):
+
+    def __str__(self):
         return '%s, %s: Bad value' % (self.key, self.value)
-class ReadError (BovError):
-    def __init__ (self, file):
+
+
+class ReadError(BovError):
+    def __init__(self, file):
         self.file = file
-    def __str__ (self):
+
+    def __str__(self):
         return '%s: Unable to read' % (self.file)
-class FileExists (BovError):
-    def __init__ (self, file):
+
+
+class FileExists(BovError):
+    def __init__(self, file):
         self.file = file
-    def __str__ (self):
+
+    def __str__(self):
         return '%s: Unable to write to file' % self.file
-class BadFileExtension (BovError):
-    def __init__ (self, ext):
+
+
+class BadFileExtension(BovError):
+    def __init__(self, ext):
         self.ext = ext
+
     def __str__ (self):
         return "%s: Extension should be '.bov' or empty" % self.ext
 
-bov_to_np_type = {'BYTE': 'uint8', 'SHORT': 'int32', 'INT': 'int64',
+
+_BOV_TO_NP_TYPE = {'BYTE': 'uint8', 'SHORT': 'int32', 'INT': 'int64',
                   'FLOAT': 'float32', 'DOUBLE': 'float64'}
-np_to_bov_type = dict (zip (bov_to_np_type.values (), bov_to_np_type.keys ()))
-sys_to_bov_endian = {'little': 'LITTLE', 'big': 'BIG'}
+_NP_TO_BOV_TYPE = dict(zip(_BOV_TO_NP_TYPE.values(), _BOV_TO_NP_TYPE.keys()))
+_SYS_TO_BOV_ENDIAN = {'little': 'LITTLE', 'big': 'BIG'}
 
-def array_to_str (array):
-    s = [str (x) for x in array]
-    return ' '.join (s)
 
-def fromfile (file, allow_singleton=True):
+def array_to_str(array):
+    s = [str(x) for x in array]
+    return ' '.join(s)
+
+
+def fromfile(file, allow_singleton=True):
     """
     >>> (grid, attrs) = fromfile ('test.bov')
     >>> grid.get_shape () #doctest: +NORMALIZE_WHITESPACE
@@ -91,33 +109,33 @@ def fromfile (file, allow_singleton=True):
 
     """
     header = {}
-    with open (file, 'r') as f:
+    with open(file, 'r') as f:
         for line in f:
             try:
-                (data, comment) = line.split ('#')
+                (data, comment) = line.split('#')
             except ValueError:
                 data = line
             try:
-                (key, value) = data.split (':')
-                header[key.strip ()] = value.strip ()
+                (key, value) = data.split(':')
+                header[key.strip()] = value.strip()
             except ValueError:
                 pass
 
-    keys_found = set (header.keys ())
-    keys_required = set (['DATA_SIZE', 'DATA_FORMAT', 'DATA_FILE',
+    keys_found = set(header.keys())
+    keys_required = set(['DATA_SIZE', 'DATA_FORMAT', 'DATA_FILE',
                          'BRICK_ORIGIN', 'BRICK_SIZE', 'VARIABLE'])
-    if not keys_required.issubset (keys_found):
-        missing = ', '.join (keys_required-keys_found)
-        raise MissingRequiredKeyError (missing)
+    if not keys_required.issubset(keys_found):
+        missing = ', '.join(keys_required-keys_found)
+        raise MissingRequiredKeyError(missing)
 
-    shape = header['DATA_SIZE'].split ()
-    header['DATA_SIZE'] = np.array ([int (i) for i in shape], dtype=np.int64)
+    shape = header['DATA_SIZE'].split()
+    header['DATA_SIZE'] = np.array([int(i) for i in shape], dtype=np.int64)
 
-    origin = header['BRICK_ORIGIN'].split ()
-    header['BRICK_ORIGIN'] = np.array ([float (i) for i in origin], dtype=np.float64)
+    origin = header['BRICK_ORIGIN'].split()
+    header['BRICK_ORIGIN'] = np.array([float(i) for i in origin], dtype=np.float64)
 
-    size = header['BRICK_SIZE'].split ()
-    header['BRICK_SIZE'] = np.array ([float (i) for i in size], dtype=np.float64)
+    size = header['BRICK_SIZE'].split()
+    header['BRICK_SIZE'] = np.array([float(i) for i in size], dtype=np.float64)
 
     if not allow_singleton:
         not_singleton = header['DATA_SIZE']>1
@@ -127,26 +145,26 @@ def fromfile (file, allow_singleton=True):
 
     type_str = header['DATA_FORMAT']
     try:
-        type = bov_to_np_type[type_str]
+        type = _BOV_TO_NP_TYPE[type_str]
     except KeyError as e:
-        raise BadKeyValueError ('DATA_FORMAT', type_str)
+        raise BadKeyValueError('DATA_FORMAT', type_str)
 
     dat_file = header['DATA_FILE']
-    if not os.path.isabs (dat_file):
-        dat_file = os.path.join (os.path.dirname (file), dat_file)
+    if not os.path.isabs(dat_file):
+        dat_file = os.path.join(os.path.dirname(file), dat_file)
 
     try:
-        data = np.fromfile (dat_file, dtype=type)
+        data = np.fromfile(dat_file, dtype=type)
     except Exception as e:
         raise 
 
     try:
         data.shape = header['DATA_SIZE']
     except ValueError as e:
-        raise BadKeyValueError ('DATA_SIZE', '%d != %d' % (np.prod (header['DATA_SIZE']), data.size))
+        raise BadKeyValueError('DATA_SIZE', '%d != %d' % (np.prod(header['DATA_SIZE']), data.size))
 
     try:
-        header['TIME'] = float (header['TIME'])
+        header['TIME'] = float(header['TIME'])
     except KeyError:
         pass
 
@@ -154,50 +172,33 @@ def fromfile (file, allow_singleton=True):
     origin = header['BRICK_ORIGIN']
     spacing = header['BRICK_SIZE']/(shape-1)
 
-    grid = RasterField (shape, spacing, origin, indexing='ij')
-    if header.has_key ('CENTERING') and header['CENTERING'] == 'zonal':
-        grid.add_field (header['VARIABLE'], data, centering='zonal')
+    grid = RasterField(shape, spacing, origin, indexing='ij')
+    if header.has_key('CENTERING') and header['CENTERING'] == 'zonal':
+        grid.add_field(header['VARIABLE'], data, centering='zonal')
     else:
-        grid.add_field (header['VARIABLE'], data, centering='point')
-
-    #grid.add_field (header['VARIABLE'], data, centering=header['
-    #if header.has_key ('CENTERING') and header['CENTERING'] == 'zonal':
-    #    grid = VTKGridUniformRectilinear (shape, spacing, origin)
-    #    #grid = BovFile (shape, spacing, origin)
-
-    #    grid.add_cell_data (header['VARIABLE'], data)
-    #    #grid.set_attrs (header)
-    #else:
-    #    size = header['BRICK_SIZE']
-    #    x = np.linspace (origin[0], origin[0]+size[0], shape[0])
-    #    y = np.linspace (origin[1], origin[1]+size[1], shape[1])
-    #    #x = np.arange (shape[0], dtype=np.float64)*spacing[0] + origin[0]
-    #    #y = np.arange (shape[1], dtype=np.float64)*spacing[1] + origin[1]
-    #    (X, Y) = np.meshgrid (x, y)
-    #    grid = VTKGridPoints (X, Y)
-    #    grid.add_point_data (header['VARIABLE'], data)
-
-    #grid = VTKGridUniformRectilinear (shape, spacing, origin)
+        grid.add_field(header['VARIABLE'], data, centering='point')
 
     return (grid, header)
 
-def array_tofile (file, array, name='', spacing=(1., 1.), origin=(0., 0.), no_clobber=False, options={}):
-    files_written = []
-    (base, ext) = os.path.splitext (file)
-    if len (ext)>0 and ext != '.bov':
-        raise BadFileExtension (ext)
 
-    spacing = np.array (spacing, dtype=np.float64)
-    origin = np.array (origin, dtype=np.float64)
-    shape = np.array (array.shape, dtype=np.int64)
+def array_tofile(file, array, name='', spacing=(1., 1.), origin=(0., 0.),
+                 no_clobber=False, options={}):
+    files_written = []
+    (base, ext) = os.path.splitext(file)
+    if len(ext) > 0 and ext != '.bov':
+        raise BadFileExtension(ext)
+
+    spacing = np.array(spacing, dtype=np.float64)
+    origin = np.array(origin, dtype=np.float64)
+    shape = np.array(array.shape, dtype=np.int64)
     size = shape * spacing
 
-    if len (shape)<3:
-        shape = np.append (shape, [1]*(3-len (shape)))
-    if len (origin)<3:
-        origin = np.append (origin, [1.]*(3-len (origin)))
-    if len (size)<3:
-        size = np.append (size, [1.]*(3-len (size)))
+    if len(shape) < 3:
+        shape = np.append(shape, [1] * (3 - len(shape)))
+    if len(origin) < 3:
+        origin = np.append(origin, [1.] * (3 - len(origin)))
+    if len(size) < 3:
+        size = np.append(size, [1.] * (3 - len(size)))
 
     vars = [(name, array)]
 
@@ -206,35 +207,32 @@ def array_tofile (file, array, name='', spacing=(1., 1.), origin=(0., 0.), no_cl
         bov_file = '%s_%s.bov' % (base, var)
 
         if no_clobber:
-            if os.path.isfile (bov_file):
-                raise FileExists (bov_file)
-            if os.path.isfile (dat_file):
-                raise FileExists (dat_file)
+            if os.path.isfile(bov_file):
+                raise FileExists(bov_file)
+            if os.path.isfile(dat_file):
+                raise FileExists(dat_file)
 
-        #try:
-        #    vals = grid.cell_data (var)
-        #except (AttributeError, TypeError):
-        #    raise TypeError ('\'%s\' object is not grid-like' % type (grid))
+        vals.tofile(dat_file)
 
-        vals.tofile (dat_file)
+        header = dict(DATA_FILE=dat_file,
+                      DATA_SIZE=array_to_str(shape),
+                      BRICK_ORIGIN=array_to_str(origin),
+                      BRICK_SIZE=array_to_str(size),
+                      DATA_ENDIAN=_SYS_TO_BOV_ENDIAN[sys.byteorder],
+                      DATA_FORMAT=_NP_TO_BOV_TYPE[str(vals.dtype)],
+                      VARIABLE=var)
 
-        header = dict (DATA_FILE=dat_file,
-                       DATA_SIZE=array_to_str (shape),
-                       BRICK_ORIGIN=array_to_str (origin),
-                       BRICK_SIZE=array_to_str (size),
-                       DATA_ENDIAN=sys_to_bov_endian[sys.byteorder],
-                       DATA_FORMAT=np_to_bov_type[str (vals.dtype)],
-                       VARIABLE=var)
+        header.update(options)
+        with open(bov_file, 'w') as f:
+            for item in header.items():
+                f.write('%s: %s\n' % item)
 
-        header.update (options)
-        with open (bov_file, 'w') as f:
-            for item in header.items ():
-                f.write ('%s: %s\n' % item)
+        files_written.append(bov_file)
 
-        files_written.append (bov_file)
     return files_written
 
-def tofile (file, grid, var_name=None, no_clobber=False, options={}):
+
+def tofile(file, grid, var_name=None, no_clobber=False, options={}):
     """
     Write a grid-like object to a BOV file.
 
@@ -253,68 +251,60 @@ def tofile (file, grid, var_name=None, no_clobber=False, options={}):
     :returns: A list of the files written.
     """
     files_written = []
-    (base, ext) = os.path.splitext (file)
-    if len (ext)>0 and ext != '.bov':
-        raise BadFileExtension (ext)
+    (base, ext) = os.path.splitext(file)
+    if len(ext) > 0 and ext != '.bov':
+        raise BadFileExtension(ext)
 
     try:
-        shape = grid.get_shape ()
-        origin = grid.get_origin ()
-        size = grid.get_shape ()*grid.get_spacing ()
+        shape = grid.get_shape()
+        origin = grid.get_origin()
+        size = grid.get_shape() * grid.get_spacing()
     except (AttributeError, TypeError):
-        raise TypeError ('\'%s\' object is not grid-like' % type (grid))
+        raise TypeError('\'%s\' object is not grid-like' % type(grid))
 
-    if len (shape)<3:
-        shape = np.append (shape, [1]*(3-len (shape)))
-    if len (origin)<3:
-        origin = np.append (origin, [1.]*(3-len (origin)))
-    if len (size)<3:
-        size = np.append (size, [1.]*(3-len (size)))
+    if len(shape) < 3:
+        shape = np.append(shape, [1] * (3 - len(shape)))
+    if len(origin) < 3:
+        origin = np.append(origin, [1.] * (3 - len(origin)))
+    if len(size) < 3:
+        size = np.append(size, [1.] * (3 - len(size)))
 
     if var_name is None:
         vars = grid.get_point_fields().items()
-        #vars = grid.items ()
     else:
         vars = (var_name, grid.get_field(var_name))
 
     for (var, vals) in vars:
-        #dat_file = '%s_%s.dat' % (base, var)
-        #bov_file = '%s_%s.bov' % (base, var)
         dat_file = '%s.dat' % (base, )
         bov_file = '%s.bov' % (base, )
 
         if no_clobber:
-            if os.path.isfile (bov_file):
-                raise FileExists (bov_file)
-            if os.path.isfile (dat_file):
-                raise FileExists (dat_file)
+            if os.path.isfile(bov_file):
+                raise FileExists(bov_file)
+            if os.path.isfile(dat_file):
+                raise FileExists(dat_file)
 
-        #try:
-        #    vals = grid.cell_data (var)
-        #except (AttributeError, TypeError):
-        #    raise TypeError ('\'%s\' object is not grid-like' % type (grid))
+        vals.tofile(dat_file)
 
-        vals.tofile (dat_file)
+        header = dict(DATA_FILE=dat_file,
+                      DATA_SIZE=array_to_str(shape),
+                      BRICK_ORIGIN=array_to_str(origin),
+                      BRICK_SIZE=array_to_str(size),
+                      DATA_ENDIAN=_SYS_TO_BOV_ENDIAN[sys.byteorder],
+                      DATA_FORMAT=_NP_TO_BOV_TYPE[str(vals.dtype)],
+                      VARIABLE=var)
 
-        header = dict (DATA_FILE=dat_file,
-                       DATA_SIZE=array_to_str (shape),
-                       BRICK_ORIGIN=array_to_str (origin),
-                       BRICK_SIZE=array_to_str (size),
-                       DATA_ENDIAN=sys_to_bov_endian[sys.byteorder],
-                       DATA_FORMAT=np_to_bov_type[str (vals.dtype)],
-                       VARIABLE=var)
+        header.update(options)
+        with open(bov_file, 'w') as opened_file:
+            for item in header.items():
+                opened_file.write('%s: %s\n' % item)
 
-        header.update (options)
-        with open (bov_file, 'w') as f:
-            for item in header.items ():
-                f.write ('%s: %s\n' % item)
+        files_written.append(bov_file)
 
-        files_written.append (bov_file)
     return files_written
 
 
-#class BovFile (VTKGridUniformRectilinear):
-class BovFile (RasterField):
+class BovFile(RasterField):
     """
     >>> bov = BovFile ((400, 300), (2., 1.), (0., 0.))
     >>> x = bov.get_x_coordinates ()
@@ -335,70 +325,64 @@ class BovFile (RasterField):
     BadFileExtension: .txt: Extension should be '.bov' or empty
     """
 
-    def __init__ (self, *args, **kwargs):
-        super (BovFile, self).__init__ (*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(BovFile, self).__init__(*args, **kwargs)
         self.attrs = {}
 
-    def tobov (self, file, no_clobber=False, options={}):
+    def tobov(self, file, no_clobber=False, options={}):
         files_written = []
-        (base, ext) = os.path.splitext (file)
-        if len (ext)>0 and ext is not '.bov':
-            raise BadFileExtension (ext)
+        (base, ext) = os.path.splitext(file)
+        if len(ext) > 0 and ext is not '.bov':
+            raise BadFileExtension(ext)
 
-        for var in self.cell_data_vars ():
+        for var in self.cell_data_vars():
             dat_file = '%s_%s.dat' % (base, var)
             bov_file = '%s_%s.bov' % (base, var)
 
             if no_clobber:
-                if os.path.isfile (bov_file):
-                    raise FileExists (bov_file)
-                if os.path.isfile (dat_file):
-                    raise FileExists (dat_file)
+                if os.path.isfile(bov_file):
+                    raise FileExists(bov_file)
+                if os.path.isfile(dat_file):
+                    raise FileExists(dat_file)
 
-            vals = self.cell_data (var)
-            vals.tofile (dat_file)
+            vals = self.cell_data(var)
+            vals.tofile(dat_file)
 
-            shape = self.get_shape ()
-            if len (shape)<3:
-                shape = np.append (shape, [1]*(3-len (shape)))
+            shape = self.get_shape()
+            if len(shape) < 3:
+                shape = np.append(shape, [1] * (3 - len(shape)))
 
-            origin = self.get_origin ()
-            if len (origin)<3:
-                origin = np.append (origin, [1.]*(3-len (origin)))
+            origin = self.get_origin()
+            if len(origin) < 3:
+                origin = np.append(origin, [1.] * (3 - len(origin)))
 
-            size = self.get_shape ()*self.get_spacing ()
-            if len (size)<3:
-                size = np.append (size, [1.]*(3-len (size)))
+            size = self.get_shape() * self.get_spacing()
+            if len(size) < 3:
+                size = np.append(size, [1.] * (3 - len(size)))
 
-            header = dict (DATA_FILE=dat_file,
-                           DATA_SIZE=array_to_str (shape),
-                           BRICK_ORIGIN=array_to_str (origin),
-                           BRICK_SIZE=array_to_str (size),
-                           DATA_ENDIAN=sys_to_bov_endian[sys.byteorder],
-                           DATA_FORMAT=np_to_bov_type[str (vals.dtype)],
-                           VARIABLE=var)
+            header = dict(DATA_FILE=dat_file,
+                          DATA_SIZE=array_to_str(shape),
+                          BRICK_ORIGIN=array_to_str(origin),
+                          BRICK_SIZE=array_to_str(size),
+                          DATA_ENDIAN=_SYS_TO_BOV_ENDIAN[sys.byteorder],
+                          DATA_FORMAT=_NP_TO_BOV_TYPE[str(vals.dtype)],
+                          VARIABLE=var)
 
-            header.update (options)
-            with open (bov_file, 'w') as f:
-                for item in header.items ():
-                    f.write ('%s: %s\n' % item)
+            header.update(options)
+            with open(bov_file, 'w') as opened_file:
+                for item in header.items():
+                    opened_file.write('%s: %s\n' % item)
 
-            files_written.append (bov_file)
+            files_written.append(bov_file)
         return files_written
 
-    def set_attrs (self, attrs):
-        self.attrs = attrs.copy ()
-    def get_attr (self, attr):
+    def set_attrs(self, attrs):
+        self.attrs = attrs.copy()
+
+    def get_attr(self, attr):
         return self.attrs[attr]
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod() 
-
-    #import cProfile
-    #cProfile.run ('doctest.testmod()', 'bov.prof')
-
-    #import pstats
-    #p = pstats.Stats ('bov.prof')
-    #p.strip_dirs ().sort_stats ('cumulative').print_stats (10)
