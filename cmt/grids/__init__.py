a5f70af5ab92f7377c17ebcc1d15c9f164a58b6b
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2013-11-01 19:35:44 +0000

    * Changed cmt.grids.__init__ to import more classes.

--- cmt/grids/__init__.py
+++ cmt/grids/__init__.py
@@ -1,8 +1,9 @@
 from cmt.grids.raster import UniformRectilinear, UniformRectilinearPoints
-from cmt.grids.rectilinear import RectilinearPoints, Rectilinear
-from cmt.grids.structured import Structured
-from cmt.grids.unstructured import Unstructured
-from cmt.grids.field import RasterField
+from cmt.grids.rectilinear import Rectilinear, RectilinearPoints
+from cmt.grids.structured import Structured, StructuredPoints
+from cmt.grids.unstructured import Unstructured, UnstructuredPoints
+from cmt.grids.field import (RasterField, RectilinearField, StructuredField,
+                             UnstructuredField)
 
 from cmt.grids.assertions import (is_uniform_rectilinear, is_rectilinear,
                                   is_structured, is_unstructured)
