3561916b09b212f7d919b4ac8d2f05c1046ca36f
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2013-11-11 19:09:32 +0000

    * Added ugrid module. Defines NetcdfField class for constructing netcdf files.
    == Added ==
    
    * ugrid module define NetcdfField base class used for construting netcdf files
      that conform to the ugrid/cf-conventions. Subclass this class to construct
      files that define different grid types. For instance: rectilinear,
      structured, unstructured, etc.

---
+++ cmt/nc/ugrid.py
@@ -0,0 +1,305 @@
+#! /usr/bin/env python
+
+import os
+
+from cmt.nc.constants import (open_netcdf, _NP_TO_NC_TYPE)
+import cmt.grids.utils as gutils
+
+
+class NetcdfField(object):
+    def __init__(self, path, field, format='NETCDF4', append=False):
+        self._path = path
+        self._field = field
+
+        self._root = open_netcdf(path, format=format, append=append)
+
+        self._set_mesh_topology()
+        self._set_node_variable_data()
+        self._set_face_variable_data()
+        self._set_time_variable()
+
+        self._root.close()
+
+    @property
+    def field(self):
+        return self._field
+
+    @property
+    def root(self):
+        return self._root
+
+    @property
+    def topology_dimension(self):
+        return len(gutils.non_singleton_shape(self.field))
+
+    @property
+    def field_axes(self):
+        return gutils.non_singleton_axes(self.field)
+
+    @property
+    def node_coordinates(self):
+        return ''
+
+    @property
+    def face_connectivity(self):
+        return 'face_nodes'
+
+    @property
+    def node_count(self):
+        return self.field.get_point_count()
+
+    @property
+    def face_count(self):
+        return self.field.get_cell_count()
+
+    @property
+    def vertex_count(self):
+        return self.field.get_vertex_count()
+
+    @property
+    def time_count(self):
+        try:
+            return len(self._root.variables['time'])
+        except KeyError:
+            return 0
+
+    def has_dimension(self, name):
+        return name in self._root.dimensions
+
+    def has_variable(self, name):
+        return name in self._root.variables
+
+    def create_dimension(self, name, dim_len):
+        try:
+            if not self.has_dimension(name):
+                self._root.createDimension(name, dim_len)
+        except IndexError:
+            pass
+
+    def create_variable(self, name, *args):
+        if not self.has_variable(name):
+            try:
+                self._root.createVariable(name, *args)
+            except ValueError as error:
+                print 'error is', error
+                print args
+                print self._root.dimensions
+                raise
+               
+    def set_variable(self, name, *args, **kwds):
+        assert(len(args) in [0, 1])
+
+        attrs = kwds.pop('attrs', {})
+
+        variable = self.data_variable(name)
+        for (attr, value) in attrs.items():
+            setattr(variable, attr, value)
+
+        if len(args) > 0:
+            array = args[0]
+            if 'time' in variable.dimensions:
+                n_times = self.time_count
+                if array.size > 1:
+                    variable[n_times,:] = array.flat
+                else:
+                    variable[n_times] = array[0]
+            else:
+                variable[:] = array.flat
+
+    def data_variable(self, name):
+        return self.root.variables[name]
+
+    def _set_mesh_topology(self):
+        self._set_topology()
+        self._set_mesh_dimensions()
+        self._set_time_dimension()
+        self._set_mesh_coordinate_data()
+        self._set_face_node_connectivity_data()
+
+    def _set_topology(self):
+        self.create_variable('mesh', 'i8')
+        self.set_variable('mesh',
+            attrs={
+                'cf_role': 'mesh_topology',
+                'topology_dimension': self.topology_dimension,
+                'node_coordinates': ' '.join(self.node_coordinates),
+                'face_connectivity': self.face_connectivity,
+            })
+
+    def _set_time_dimension(self):
+        if not self.has_dimension('time'):
+            self.create_dimension('time', None)
+
+    def _set_coordinate_data(self):
+        self._set_mesh_coordinate_data()
+        self._set_face_node_connectivity_data()
+
+    def _set_time_variable(self, now=None, units='days',
+                           reference='00:00:00 UTC'):
+        self.create_variable('time', 'f8', ('time', ))
+
+        time = self.data_variable('time')
+        time.units = ' '.join ([units, 'since', reference])
+        time.long_name = 'time'
+
+        if now is not None:
+            time[self.time_count - 1] = now
+        else:
+            time[self.time_count - 1] = self.time_count - 1
+
+    def _set_variable_data(self):
+        self._set_node_variable_data()
+        self._set_face_variable_data()
+
+    def _set_node_variable_data(self):
+        point_fields = self.field.get_point_fields()
+        for (var_name, array) in point_fields.items():
+            self.create_variable(var_name, _NP_TO_NC_TYPE[str(array.dtype)],
+                                 ['time'] + list(self.node_data_dimensions))
+            self.set_variable(
+                var_name, array,
+                attrs={
+                    'units': self.field.get_field_units(var_name),
+                    'standard_name': var_name,
+                    'long_name': var_name,
+                    'location': 'node',
+                    'coordinates': ' '.join(self.node_data_dimensions)
+                })
+
+    def _set_face_variable_data(self):
+        face_fields = self.field.get_cell_fields()
+        for (var_name, array) in face_fields.items():
+            self.create_variable(var_name, _NP_TO_NC_TYPE[str(array.dtype)],
+                                 ['time', ] + list(self.face_data_dimensions))
+            self.set_variable(
+                var_name, array,
+                attrs={
+                    'units': self.field.get_field_units(var_name),
+                    'standard_name': var_name,
+                    'long_name': var_name,
+                    'location': 'face',
+                    'coordinates': ' '.join(self.node_data_dimensions)
+                })
+
+    def _set_face_node_connectivity_data(self):
+        pass
+
+
+class NetcdfRectilinearField(NetcdfField):
+    @property
+    def topology_dimension(self):
+        return len(gutils.non_singleton_shape(self.field))
+
+    @property
+    def node_data_dimensions(self):
+        return gutils.non_singleton_dimension_names(self.field)
+
+    @property
+    def axis_coordinates(self):
+        return gutils.non_singleton_dimension_names(self.field)
+
+    def _set_mesh_dimensions(self):
+        field_shape = self.field.get_shape()
+        for (name, axis) in zip(self.axis_coordinates, self.field_axes):
+            self.create_dimension(name, field_shape[axis])
+
+    def _set_mesh_coordinate_data(self):
+        for (name, axis) in zip(self.axis_coordinates, self.field_axes):
+            self.create_variable(name, 'f8', (name, ))
+            self.set_variable(
+                name, self.field.get_axis_coordinates(axis=axis),
+                attrs={'units': self.field.get_coordinate_units(axis),
+                       'standard_name': self.field.get_coordinate_name(axis),
+                       'long_name': self.field.get_coordinate_name(axis),
+                       'name': self.field.get_coordinate_name(axis),
+                      })
+
+
+class NetcdfStructuredField(NetcdfRectilinearField):
+    @property
+    def node_data_dimensions(self):
+        return gutils.non_singleton_dimension_names(self._field)
+
+    @property
+    def node_coordinates(self):
+        names = gutils.non_singleton_dimension_names(self._field)
+        return ['node_' + name for name in names]
+
+    def _set_mesh_dimensions(self):
+        NetcdfRectilinearField._set_mesh_dimensions(self)
+
+        for name in self.node_coordinates:
+            self.create_dimension(name, self.node_count)
+
+    def _set_mesh_coordinate_data(self):
+        dims = self.node_data_dimensions
+        #for (name, axis) in zip(self.node_coordinates, self.field_axes):
+        for (name, axis) in zip(self.node_data_dimensions, self.field_axes):
+            self.create_variable(name, 'f8', dims)
+            self.set_variable(
+                name, self.field.get_coordinate(axis),
+                attrs={'units': self.field.get_coordinate_units(axis),
+                       'standard_name': self.field.get_coordinate_name(axis),
+                       'long_name': self.field.get_coordinate_name(axis),
+                      })
+
+
+class NetcdfUnstructuredField(NetcdfStructuredField):
+    @property
+    def node_coordinates(self):
+        names = []
+        for axis in xrange(self.field.get_dim_count()):
+            names.append('node_' + self.field.get_coordinate_name(axis))
+        return names
+
+    @property
+    def topology_dimension(self):
+        return self.field.get_dim_count()
+
+    @property
+    def node_data_dimensions(self):
+        return ('n_node', )
+        #dimensions = []
+        #for name in self.node_coordinates:
+        #    dimensions.append((name, ))
+        #return dimensions
+
+    @property
+    def face_data_dimensions(self):
+        return ('n_face', )
+
+    def _set_mesh_dimensions(self):
+        #for name in self.node_coordinates:
+        #    self.create_dimension(name, self.node_count)
+
+        self.create_dimension('n_node', self.node_count)
+        self.create_dimension('n_face', self.face_count)
+        self.create_dimension('n_vertex', self.vertex_count)
+
+    def _set_mesh_coordinate_data(self):
+        dims = self.node_data_dimensions
+        #for (name, axis) in zip(self.node_data_dimensions, self.field_axes):
+        #for (name, axis) in zip(self.node_coordinates, self.field_axes):
+        for (axis, name) in enumerate(self.node_coordinates):
+            self.create_variable(name, 'f8', dims)
+            self.set_variable(
+                name, self.field.get_coordinate(axis),
+                attrs={'units': self.field.get_coordinate_units(axis),
+                       'standard_name': self.field.get_coordinate_name(axis),
+                       'long_name': self.field.get_coordinate_name(axis),
+                      })
+
+    def _set_face_node_connectivity_data(self):
+        self.create_variable('face_nodes', 'i8', ('n_vertex', ))
+        self.set_variable(
+            'face_nodes', self.field.get_connectivity(),
+            attrs={'cf_role': 'face_node_connectivity',
+                   'long_name': 'Maps every face to its corner nodes.'
+                  })
+
+        self.create_variable('face_nodes_offset', 'i8', ('n_face', ))
+        self.set_variable(
+            'face_nodes_offset', self.field.get_offset(),
+            attrs={'cf_role': 'face_node_offset',
+                   'long_name': 'Maps face index into connectivity array'
+                  })
