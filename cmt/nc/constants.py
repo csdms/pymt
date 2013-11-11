fb3b803b46df864fd8c9f6f07f55a810b65d24ed
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2013-11-11 19:06:16 +0000

    * Added constants.py module to hold netcdf-specific constants.

---
+++ cmt/nc/constants.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+
+_VALID_NETCDF_FORMATS = set([
+    'NETCDF3_CLASSIC',
+    'NETCDF3_64BIT',
+])
+
+_NP_TO_NC_TYPE = {
+    'float32': 'f4',
+    'float64': 'f8',
+    'int8': 'i1',
+    'int16': 'i2',
+    'int32': 'i4',
+    'int64': 'i8',
+    'uint8': 'u1',
+    'uint16': 'u2',
+    'uint32': 'u4',
+    'uint64': 'u8',
+}
+
+import os
+from scipy.io import netcdf as nc3
+try:
+    import netCDF4 as nc4
+except ImportError:
+    warnings.warn('Unable to import netCDF4.', ImportWarning)
+else:
+    _VALID_NETCDF_FORMATS |= set(['NETCDF4_CLASSIC', 'NETCDF4'])
+
+
+def assert_valid_netcdf_format(format):
+    if format not in _VALID_NETCDF_FORMATS:
+        raise ValueError('%s: format is one of %s' %
+                         (format, ', '.join(_VALID_NETCDF_FORMATS)))
+
+
+def open_netcdf(path, mode='r', format='NETCDF3_CLASSIC', append=False):
+    assert_valid_netcdf_format(format)
+
+    if os.path.isfile(path) and append:
+        mode = 'a'
+    else:
+        mode = 'w'
+
+    if format == 'NETCDF3_CLASSIC':
+        root = nc3.netcdf_file(path, mode, version=1)
+    elif format == 'NETCDF3_64BIT':
+        root = nc3.netcdf_file(path, mode, version=2)
+    else:
+        root = nc4.Dataset(path, mode, format=format)
+
+    return root
