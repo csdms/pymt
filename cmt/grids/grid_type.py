37fdd48db109f53e0c797c1d733058ff07d989d1
huttone <huttone@c6590863-6ce9-4c6c-9238-e75f74d085ca>
2013-11-18 23:23:43 +0000

    * Added classes that define a type of grid.
    == Added ==
    
    * Added new classes:
      - GridType
      - GridTypeRectilinear
      - GridTypeStructured
      - GridTypeUnstructured

---
+++ cmt/grids/grid_type.py
@@ -0,0 +1,22 @@
+
+class GridType(object):
+    def __eq__(self, that):
+        return isinstance(that, self.__class__) or str(self) == str(that)
+
+    def __str__(self):
+        return self._type
+
+    def __repr__(self):
+        return '%s()' % self.__class__.__name__
+
+
+class GridTypeRectilinear(GridType):
+    _type = 'rectilinear'
+
+
+class GridTypeStructured(GridType):
+    _type = 'structured'
+
+
+class GridTypeUnstructured(GridType):
+    _type = 'unstructured'
