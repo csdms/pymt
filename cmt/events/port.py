d7a9d117a66118563dd5621687f94e0330a1a979
mcflugen <mcflugen@gmail.com>
2014-03-31 17:10:02 -0600

    Added PortEvent and unit tests.
    Added
    =====
    
    * PortEvent class is an wrapper for a generic IRF port.
    * New unit tests to make sure it works within an EventManager.

---
+++ cmt/events/port.py
@@ -0,0 +1,16 @@
+from cmt.printqueue.port_printer import PortPrinter
+import services
+
+
+class PortEvent(object):
+    def __init__(self, *args, **kwds):
+        self._port = services.get_port(kwds['port'])
+
+    def initialize(self):
+        self._port.initialize()
+
+    def run(self, time):
+        self._port.run(time)
+
+    def finalize(self):
+        self._port.finalize()
