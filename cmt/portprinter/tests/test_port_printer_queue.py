db461a1a577488057b67bd10238db2d510e8b53e
mcflugen <mcflugen@gmail.com>
2014-04-01 13:20:11 -0600

    Added unit tests for multiple printers.

--- cmt/portprinter/tests/test_port_printer_queue.py
+++ cmt/portprinter/tests/test_port_printer_queue.py
@@ -1,53 +1,49 @@
 #! /usr/bin/env python
 
-import cmt.printqueue.port_printer_queue as ppq
+from nose.tools import assert_is_instance
+
+from cmt.portprinter.port_printer import PortPrinter
 from cmt.testing.ports import UniformRectilinearGridPort
 from cmt.testing.assertions import assert_isfile_and_remove
 
 
-def test_uniform_rectilinear_vtk():
-    port = UniformRectilinearGridPort()
+def test_one_printer():
+    queue = PortPrinter.from_string("""
+[print.air__density]
+format=vtk
+port=uniform_rectilinear
+""")
+    assert_is_instance(queue, PortPrinter)
 
-    queue = ppq.PortPrinterQueue(port)
-    queue.append('air__density', format='vtk')
     queue.open()
+    queue.write()
+
+    assert_isfile_and_remove('air__density_0000.vtu')
+
+
+def test_multiple_printers():
+    queue = PortPrinter.from_string("""
+[print.air__density]
+format=vtk
+port=uniform_rectilinear
+
+[print.glacier_top_surface__slope]
+format=nc
+port=uniform_rectilinear
+""")
+
+    assert_is_instance(queue, list)
+
+    for printer in queue:
+        printer.open()
+
     for _ in xrange(5):
-        queue.write()
-    queue.close()
+        for printer in queue:
+            printer.write()
 
     assert_isfile_and_remove('air__density_0000.vtu')
     assert_isfile_and_remove('air__density_0001.vtu')
     assert_isfile_and_remove('air__density_0002.vtu')
     assert_isfile_and_remove('air__density_0003.vtu')
     assert_isfile_and_remove('air__density_0004.vtu')
-
-
-def test_uniform_rectilinear_nc():
-    port = UniformRectilinearGridPort()
-
-    queue = ppq.PortPrinterQueue(port)
-    queue.append('glacier_top_surface__slope', format='nc')
-    queue.open()
-    for _ in xrange(5):
-        queue.write()
-    queue.close()
-
     assert_isfile_and_remove('glacier_top_surface__slope.nc')
-
-
-def test_from_string():
-    port = UniformRectilinearGridPort()
-    ini_string = """
-[print.air__density]
-format=vtk
-
-[print.glacier_top_surface__slope]
-format=nc
-    """
-    queue = ppq.PortPrinterQueue.from_string(ini_string, port)
-    queue.open()
-    queue.write()
-    queue.close()
-
-    assert_isfile_and_remove('glacier_top_surface__slope.nc')
-    assert_isfile_and_remove('air__density_0000.vtu')
