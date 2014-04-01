c1ebcdb5661770821d705edee66cf2dfdcef9a0b
mcflugen <mcflugen@gmail.com>
2014-04-01 10:05:33 -0600

    Added framework subpackage.
    Added
    =====
    
    * New sub-package, framework. Right now it just contains a set_services
      function.

---
+++ cmt/framework/__init__.py
@@ -0,0 +1,1 @@
+from .services import set_services
---
+++ cmt/framework/services.py
@@ -0,0 +1,18 @@
+import sys
+import imp
+
+
+def set_services(name, path=None, ifset='pass'):
+    if 'services' in sys.modules:
+        if ifset == 'pass':
+            return
+        elif ifset == 'raise':
+            raise RuntimeError('services has already been set')
+        elif ifset == 'clobber':
+            pass
+        else:
+            raise ValueError('unrecognized option for ifset keyword')
+
+    (file, pathname, description) = imp.find_module(name, path)
+    mod = imp.load_module(name, file, pathname, description)
+    sys.modules['services'] = mod
