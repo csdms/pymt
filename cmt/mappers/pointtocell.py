8c302f6ff4d901f6cab8443d001d6bf58629bd08
mcflugen <mcflugen@gmail.com>
2014-04-02 14:15:10 -0600

    Changed styling to PEP8.

--- cmt/mappers/pointtocell.py
+++ cmt/mappers/pointtocell.py
@@ -5,51 +5,46 @@ from scipy.spatial import KDTree
 from imapper import IGridMapper
 from cmt.mappers import IncompatibleGridError
 
-class PointToCell (IGridMapper):
-    def initialize (self, dst_grid, src_grid):
-        if not self.test (dst_grid, src_grid):
-            raise IncompatibleGridError (dst_grid.name,
-                                         src_grid.name)
-        src_x = src_grid.get_x ()
-        src_y = src_grid.get_y ()
 
-        tree = KDTree (zip (dst_grid.get_x (), dst_grid.get_y ()))
-        (l, dst_point_ids) = tree.query (zip (src_x, src_y))
+class PointToCell(IGridMapper):
+    def initialize(self, dst_grid, src_grid):
+        if not self.test(dst_grid, src_grid):
+            raise IncompatibleGridError(dst_grid.name, src_grid.name)
+        src_x = src_grid.get_x()
+        src_y = src_grid.get_y()
+
+        tree = KDTree(zip(dst_grid.get_x(), dst_grid.get_y()))
+        (_, dst_point_ids) = tree.query(zip(src_x, src_y))
 
         self._map = {}
-        for (j, point_id) in zip (range (src_grid.get_point_count ()),
-                                  dst_point_ids):
-            for cell_id in dst_grid.get_shared_cells (point_id):
-                if dst_grid.is_in_cell (src_x[j], src_y[j], cell_id):
+        for (j, point_id) in zip(range(src_grid.get_point_count()),
+                                 dst_point_ids):
+            for cell_id in dst_grid.get_shared_cells(point_id):
+                if dst_grid.is_in_cell(src_x[j], src_y[j], cell_id):
                     try:
-                        self._map[cell_id].append (j)
+                        self._map[cell_id].append(j)
                     except KeyError:
                         self._map[cell_id] = [j]
 
-        self._dst_cell_count = dst_grid.get_cell_count ()
-        self._src_point_count = src_grid.get_point_count ()
+        self._dst_cell_count = dst_grid.get_cell_count()
+        self._src_point_count = src_grid.get_point_count()
 
-    def run (self, src_values, dst_vals=None, bad_val=-999, method=np.mean):
-        assert (src_values.size == self._src_point_count)
+    def run(self, src_values, dst_vals=None, bad_val=-999, method=np.mean):
+        assert(src_values.size == self._src_point_count)
+
         if dst_vals is None:
-            dst_vals = np.array ([bad_val]*self._dst_cell_count, dtype=np.float)
-        assert (dst_vals.size == self._dst_cell_count)
+            dst_vals = np.array([bad_val] * self._dst_cell_count,
+                                dtype=np.float)
+        assert(dst_vals.size == self._dst_cell_count)
 
-        for (cell_id, point_ids) in self._map.items ():
-            if all (src_values[point_ids]>bad_val):
-                dst_vals[cell_id] = method (src_values[point_ids])
+        for (cell_id, point_ids) in self._map.items():
+            if all(src_values[point_ids] > bad_val):
+                dst_vals[cell_id] = method(src_values[point_ids])
+
         return dst_vals
 
     def test (self, dst_grid, src_grid):
-        #return (all (np.diff (src_grid.get_offset ())==1) and
-        #        all (np.diff (dst_grid.get_offset ())>2))
         return all (np.diff (dst_grid.get_offset ())>2)
-
 
     def name (self):
         return 'PointToCell'
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod (optionflags=doctest.NORMALIZE_WHITESPACE)
-
