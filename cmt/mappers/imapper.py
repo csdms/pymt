7c51a01b31f8542d7e269db4676d642985a2b268
mcflugen <mcflugen@gmail.com>
2014-04-02 11:17:29 -0600

    Changed styling to be PEP8.

--- cmt/mappers/imapper.py
+++ cmt/mappers/imapper.py
@@ -1,34 +1,43 @@
 #! /bin/env python
 
-class MapperError (Exception):
+class MapperError(Exception):
     """Base class for error in this package."""
     pass
 
-class IncompatibleGridError (MapperError):
-    """Error to indicate that the source grid cannot be mapped to the destination."""
-    def __init__ (self, dst, src):
+
+class IncompatibleGridError(MapperError):
+    """Error to indicate that the source grid cannot be mapped to the
+    destination.
+    """
+    def __init__(self, dst, src):
         self._src = src
         self._dst = dst
-    def __str__ (self):
+
+    def __str__(self):
         return 'Unable to map %s to %s' % (self._src, self._dst)
 
-class NoMapperError (MapperError):
-    def __init__ (self, dst, src):
+
+class NoMapperError(MapperError):
+    def __init__(self, dst, src):
         self._src = src
         self._dst = dst
-    def __str__ (self):
+    def __str__(self):
         return 'No mapper to map %s to %s' % (self._src, self._dst)
 
-class IGridMapper (object):
+
+class IGridMapper(object):
     """Interface for a grid mapper."""
-    def initialize (self, dest_grid, src_grid):
-        """Initialize the mapper to map from a source grid to a destination grid."""
+    def initialize(self, dest_grid, src_grid):
+        """Initialize the mapper to map from a source grid to a destination
+        grid.
+        """
         pass
-    def run (self, src_values):
+
+    def run(self, src_values):
         """Map values on the source grid to the destination grid."""
         pass
+
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod (optionflags=doctest.NORMALIZE_WHITESPACE)
-
--- cmt/mappers/tests/test_mapper.py
+++ cmt/mappers/tests/test_mapper.py
@@ -1,150 +1,125 @@
 #! /usr/bin/env python
 
-import unittest
-from cmt.grids.map import RectilinearMap as Rectilinear
-from cmt.mappers import find_mapper
 import numpy as np
+from numpy.testing import assert_array_equal
+from nose.tools import assert_equal
 
-class TestMapper (unittest.TestCase):
+from cmt.grids.map import RectilinearMap as Rectilinear
+from cmt.mappers import find_mapper, NearestVal, CellToPoint, PointToCell
 
-    def test_point_to_point (self):
-        src = Rectilinear ([0, 1, 2], [0, 2])
-        dst = Rectilinear ([.5, 1.5, 2.5], [.25, 1.25])
+from cmt.grids.map import (UniformRectilinearMap as UniformRectilinear)
+from cmt.grids.map import (UnstructuredPointsMap as UnstructuredPoints)
 
-        src_vals = np.arange (src.get_point_count ())
 
-        from cmt.mappers import NearestVal
+def test_point_to_point():
+    src = Rectilinear([0, 1, 2], [0, 2])
+    dst = Rectilinear([.5, 1.5, 2.5], [.25, 1.25])
 
-        mapper = NearestVal ()
-        mapper.initialize (dst, src)
-        dst_vals = mapper.run (src_vals)
+    src_vals = np.arange(src.get_point_count ())
 
-        self.assertListEqual (list (dst_vals), list ([0., 1., 2., 3., 4., 5.]))
+    mapper = NearestVal()
+    mapper.initialize(dst, src)
+    dst_vals = mapper.run(src_vals)
 
-        mappers = find_mapper (dst, src)
-        self.assertEqual (len (mappers), 3)
-        self.assertEqual (mappers[0].name (), 'PointToPoint')
+    assert_array_equal(dst_vals, np.array([0., 1., 2., 3., 4., 5.]))
 
-        src_vals[2] = -999
-        dst_vals = np.zeros (dst.get_point_count ())-1
-        dummy = mapper.run (src_vals, dst_vals)
+    mappers = find_mapper(dst, src)
+    assert_equal(len(mappers), 3)
+    assert_equal(mappers[0].name(), 'PointToPoint')
 
-        self.assertListEqual (list (dst_vals), list ([0., 1., -1., 3., 4., 5.]))
+    src_vals[2] = -999
+    dst_vals = np.zeros(dst.get_point_count()) - 1
+    _ = mapper.run(src_vals, dst_vals)
 
-    def test_cell_to_point (self):
-        # The source grid looks like,
-        #
-        #    (0) ------ (1)
-        #     |          |
-        #     |          |
-        #    (2) ------ (3)
-        #     |          |
-        #     |          |
-        #    (4) ------ (5)
-        #     |          |
-        #     |          |
-        #    (7) ------ (7)
-        #
+    assert_array_equal(dst_vals, np.array([0., 1., -1., 3., 4., 5.]))
 
-        from cmt.mappers import CellToPoint
-        from cmt.grids.map import (UniformRectilinearMap as
-                                   UniformRectilinear)
-        from cmt.grids.map import (UnstructuredPointsMap as
-                                   UnstructuredPoints)
 
-        (dst_x, dst_y) = (np.array ([.45, 1.25, 3.5]),
-                          np.array ([.75, 2.25, 3.25]))
+def test_cell_to_point():
+    # The source grid looks like,
+    #
+    #    (0) ------ (1)
+    #     |          |
+    #     |          |
+    #    (2) ------ (3)
+    #     |          |
+    #     |          |
+    #    (4) ------ (5)
+    #     |          |
+    #     |          |
+    #    (7) ------ (7)
+    #
 
-        src = UniformRectilinear ((2,4), (2, 1), (0, 0))
-        dst = UnstructuredPoints (dst_x, dst_y)
+    (dst_x, dst_y) = (np.array([.45, 1.25, 3.5]),
+                      np.array([.75, 2.25, 3.25]))
 
-        src_vals = np.arange (src.get_cell_count (), dtype=np.float)
+    src = UniformRectilinear((2,4), (2, 1), (0, 0))
+    dst = UnstructuredPoints(dst_x, dst_y)
 
-        mapper = CellToPoint ()
-        mapper.initialize (dst, src)
-        dst_vals = mapper.run (src_vals, bad_val=-999)
+    src_vals = np.arange(src.get_cell_count(), dtype=np.float)
 
-        self.assertListEqual (list (dst_vals), [0., 2., -999.])
+    mapper = CellToPoint()
+    mapper.initialize(dst, src)
+    dst_vals = mapper.run(src_vals, bad_val=-999)
 
-        src_vals = np.arange (src.get_cell_count (), dtype=np.float)
-        src_vals[0] = -9999
-        dst_vals = np.zeros (dst.get_point_count ()) + 100
-        dummy = mapper.run (src_vals, dst_vals)
+    assert_array_equal(dst_vals, [0., 2., -999.])
 
-        self.assertListEqual (list (dst_vals), [100., 2., -999.])
+    src_vals = np.arange(src.get_cell_count(), dtype=np.float)
+    src_vals[0] = -9999
+    dst_vals = np.zeros(dst.get_point_count ()) + 100
+    _ = mapper.run(src_vals, dst_vals)
 
-    def test_point_to_cell (self):
-        from cmt.mappers import PointToCell
-        from cmt.grids.map import (UniformRectilinearMap as
-                                   UniformRectilinear)
-        from cmt.grids.map import (UnstructuredPointsMap as
-                                   UnstructuredPoints)
+    assert_array_equal(dst_vals, [100., 2., -999.])
 
-        (src_x, src_y) = (np.array ([.45, 1.25, 3.5, .0, 1.]),
-                          np.array ([.75, 2.25, 3.25, .9, 1.1]))
 
-        src = UnstructuredPoints (src_x, src_y)
-        dst = UniformRectilinear ((2,4), (2, 1), (0, 0))
+def test_point_to_cell():
+    (src_x, src_y) = (np.array([.45, 1.25, 3.5, .0, 1.]),
+                      np.array([.75, 2.25, 3.25, .9, 1.1]))
 
-        src_vals = np.arange (src.get_point_count (), dtype=np.float)
+    src = UnstructuredPoints(src_x, src_y)
+    dst = UniformRectilinear((2,4), (2, 1), (0, 0))
 
-        mapper = PointToCell ()
-        mapper.initialize (dst, src)
+    src_vals = np.arange(src.get_point_count(), dtype=np.float)
 
-        dst_vals = mapper.run (src_vals, bad_val=-999)
-        self.assertListEqual (list (dst_vals), [1.5, 4., 1.])
+    mapper = PointToCell()
+    mapper.initialize(dst, src)
 
-        dst_vals = mapper.run (src_vals, bad_val=-999, method=np.sum)
-        self.assertListEqual (list (dst_vals), [3., 4., 1.])
+    dst_vals = mapper.run(src_vals, bad_val=-999)
+    assert_array_equal(dst_vals, [1.5, 4., 1.])
 
-        src_vals[0] = -9999
-        dst_vals = np.zeros (dst.get_cell_count ()) - 1
-        dummy = mapper.run (src_vals, dst_vals)
-        self.assertListEqual (list (dst_vals), [-1., 4., 1.])
+    dst_vals = mapper.run(src_vals, bad_val=-999, method=np.sum)
+    assert_array_equal(dst_vals, [3., 4., 1.])
 
-    def test_point_to_cell_on_edges (self):
-        from cmt.mappers import PointToCell
-        from cmt.grids.map import (UniformRectilinearMap as
-                                   UniformRectilinear)
-        from cmt.grids.map import (UnstructuredPointsMap as
-                                   UnstructuredPoints)
+    src_vals[0] = -9999
+    dst_vals = np.zeros(dst.get_cell_count()) - 1
+    _ = mapper.run(src_vals, dst_vals)
+    assert_array_equal(dst_vals, [-1., 4., 1.])
 
-        (src_x, src_y) = (np.array ([0, .5, 1., 2, 3.5]),
-                          np.array ([1., 1., .0, 3, 3.]))
-        src = UnstructuredPoints (src_x, src_y)
-        dst = UniformRectilinear ((2,4), (2, 1), (0, 0))
 
-        mapper = PointToCell ()
-        mapper.initialize (dst, src)
+def test_point_to_cell_on_edges():
+    (src_x, src_y) = (np.array ([0, .5, 1., 2, 3.5]),
+                      np.array ([1., 1., .0, 3, 3.]))
+    src = UnstructuredPoints(src_x, src_y)
+    dst = UniformRectilinear((2,4), (2, 1), (0, 0))
 
-        src_vals = np.arange (src.get_point_count (), dtype=np.float)
-        dst_vals = np.zeros (dst.get_cell_count ())-1
-        dummy = mapper.run (src_vals, dst_vals)
-        self.assertListEqual (list (dst_vals), [1., 0.5, 3.])
+    mapper = PointToCell()
+    mapper.initialize(dst, src)
 
-    def test_point_to_cell_big (self):
-        from cmt.mappers import PointToCell
-        from cmt.grids.map import (UniformRectilinearMap as
-                                   UniformRectilinear)
-        from cmt.grids.map import (UnstructuredPointsMap as
-                                   UnstructuredPoints)
-        (m, n) = (20, 40)
-        (src_x, src_y) = np.meshgrid (range (m), range (n))
-        src = UnstructuredPoints (src_y, src_x)
-        dst = UniformRectilinear ((n+1,m+1), (1, 1), (-.5, -.5))
+    src_vals = np.arange(src.get_point_count(), dtype=np.float)
+    dst_vals = np.zeros(dst.get_cell_count ())-1
+    dummy = mapper.run(src_vals, dst_vals)
+    assert_array_equal(dst_vals, [1., 0.5, 3.])
 
-        mapper = PointToCell ()
-        mapper.initialize (dst, src)
 
-        src_vals = np.arange (src.get_point_count (), dtype=np.float)
-        dst_vals = np.zeros (dst.get_cell_count (), dtype=np.float)-1
-        dummy = mapper.run (src_vals, dst_vals)
-        self.assertListEqual (list (dst_vals), list (src_vals))
+def test_point_to_cell_big():
+    (m, n) = (20, 40)
+    (src_x, src_y) = np.meshgrid(range(m), range(n))
+    src = UnstructuredPoints(src_y, src_x)
+    dst = UniformRectilinear((n + 1,m + 1), (1, 1), (-.5, -.5))
 
-def suite ():
-    suite = unittest.TestLoader ().loadTestsFromTestCase (TestMapper)
-    return suite
+    mapper = PointToCell()
+    mapper.initialize(dst, src)
 
-if __name__ == '__main__':
-    unittest.main ()
-
+    src_vals = np.arange(src.get_point_count(), dtype=np.float)
+    dst_vals = np.zeros(dst.get_cell_count(), dtype=np.float) - 1
+    dummy = mapper.run(src_vals, dst_vals)
+    assert_array_equal(dst_vals, src_vals)
