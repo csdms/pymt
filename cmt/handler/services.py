7b03c7f7563097a9fce9789eccf082d69c6163e2
mcflugen <mcflugen@gmail.com>
2014-04-03 12:05:45 -0600

    Removed unused handler subpackage.

--- cmt/handler/services.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-class FrameworkServices(object):
-    def getPort(self, port_name):
-        raise NotImplementedError()
-
-    def releasePort(self, port_name):
-        raise NotImplementedError()
