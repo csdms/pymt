276270ea28eea902ed24dff926062b5cfa8ec51c
mcflugen <mcflugen@gmail.com>
2014-04-03 11:12:05 -0600

    Added requirements.txt file.
    Added
    =====
    
    * Use requirements.txt file to hold package requirements. setup.py reads
      from this file to get install_requires, and setup_requires.

--- setup.py
+++ setup.py
@@ -4,20 +4,39 @@ use_setuptools ()
 
 from setuptools import setup, find_packages
 
+from cmt import __version__
+
+
+def read_requirements():
+    import os
+
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    requirements_file = os.path.join(path, 'requirements.txt')
+    try:
+        with open(requirements_file, 'r') as req_fp:
+            requires = req_fp.read().split()
+    except IOError:
+        return []
+    else:
+        return [require.split() for require in requires]
+
 
 setup(name='Cmt',
-      version='0.1',
+      version=__version__,
       description='The Component Modeling Tools',
       author='Eric Hutton',
       author_email='huttone@colorado.edu',
       url='http://csdms.colorado.edu',
-      install_requires = ['Shapely'],
+      install_requires=read_requirements(),
+      setup_requires=read_requirements(),
       packages=find_packages(),
-      scripts=['scripts/scrape_html_block',
-               'scripts/vtu2ncu.py',
-               'scripts/quickstart.py',
-               'scripts/prmscan.py',
-               'scripts/prm2template.py',
-               'scripts/prm2input.py',
-              ],
+      scripts=[
+          'scripts/scrape_html_block',
+          'scripts/vtu2ncu.py',
+          'scripts/quickstart.py',
+          'scripts/prmscan.py',
+          'scripts/prm2template.py',
+          'scripts/prm2input.py',
+      ],
 )
