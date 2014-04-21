e43f5f9b9757281b5eed221ffb0b1a6e4e47e178
mcflugen <mcflugen@gmail.com>
2014-04-21 10:57:45 -0600

    Changed to use relative imports.

--- cmt/grids/__init__.py
+++ cmt/grids/__init__.py
@@ -1,12 +1,11 @@
-from cmt.grids.raster import UniformRectilinear, UniformRectilinearPoints
-from cmt.grids.rectilinear import Rectilinear, RectilinearPoints
-from cmt.grids.structured import Structured, StructuredPoints
-from cmt.grids.unstructured import Unstructured, UnstructuredPoints
-from cmt.grids.field import (RasterField, RectilinearField, StructuredField,
-                             UnstructuredField)
+from .raster import UniformRectilinear, UniformRectilinearPoints
+from .rectilinear import Rectilinear, RectilinearPoints
+from .structured import Structured, StructuredPoints
+from .unstructured import Unstructured, UnstructuredPoints
+from .field import (RasterField, RectilinearField, StructuredField,
+                    UnstructuredField)
 
-from cmt.grids.assertions import (is_uniform_rectilinear, is_rectilinear,
-                                  is_structured, is_unstructured)
+from .assertions import (is_uniform_rectilinear, is_rectilinear, is_structured,
+                         is_unstructured)
 
-from cmt.grids.igrid import DimensionError
-
+from .igrid import DimensionError
