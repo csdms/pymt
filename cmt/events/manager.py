21c5964eedfc2980a0fd9e8536632ba698bb927b
mcflugen <mcflugen@gmail.com>
2014-04-21 11:16:38 -0600

    Changed to recursive calls to the manager.
    Changed
    =======
    
    * The initialize, run, and finalize methods are now wrapped to prevent
      recursive calls to the manager to enter an infinite loop.

--- cmt/events/manager.py
+++ cmt/events/manager.py
@@ -5,23 +5,32 @@ from ..utils.prefix import names_with_prefix, strip_prefix
 class EventManager(object):
     def __init__(self, events):
         self._timeline = Timeline(events)
+        self._initializing = False
         self._initialized = False
+        self._running = False
+        self._finalizing = False
 
     def initialize(self):
-        if not self._initialized:
+        if not self._initializing:
+            self._initializing = True
             for event in self._timeline.events:
                 event.initialize()
             self._initialized = True
 
     def run(self, stop_time):
         self.initialize()
-        for event in self._timeline.iter_until(stop_time):
-            event.run(self._timeline.time)
+        if not self._running:
+            self._running = True
+            for event in self._timeline.iter_until(stop_time):
+                event.run(self._timeline.time)
+            self._running = False
 
     def finalize(self):
         if self._initialized:
-            for event in self._timeline.events:
-                event.finalize()
+            if not self._finalizing:
+                self._finalizing = True
+                for event in self._timeline.events:
+                    event.finalize()
             self._initialized = False
 
     def add_recurring_event(self, event, interval):
