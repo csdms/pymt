48ec4a6352a7c3cf7ae4f871d9a31287feb0643e
mcflugen <mcflugen@gmail.com>
2014-04-25 10:12:38 -0600

    Added option to warn if services are already set.

--- cmt/framework/services.py
+++ cmt/framework/services.py
@@ -1,11 +1,14 @@
 import sys
 import imp
+import warnings
 
 
 def set_services(name, path=None, ifset='pass'):
     if 'services' in sys.modules:
         if ifset == 'pass':
             return
+        elif ifset == 'warn':
+            warnings.warn('services has already been set')
         elif ifset == 'raise':
             raise RuntimeError('services has already been set')
         elif ifset == 'clobber':
