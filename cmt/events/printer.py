21a6bf19ddca49438968823d4ce002c0ccdb9330
mcflugen <huttone@colorado.edu>
2014-05-27 13:08:27 -0600

    Changed to keep output files open until finalization.
    Changed
    -------
    * Keep files open until they are closed with finalize method. This
      fixes a problem where the system was running out of file handles
      when there were lots or writes.

--- cmt/events/printer.py
+++ cmt/events/printer.py
@@ -1,15 +1,22 @@
 from ..portprinter.port_printer import PortPrinter
+from ..utils.run_dir import open_run_dir
 
 
 class PrintEvent(object):
     def __init__(self, *args, **kwds):
-        self._printer = PortPrinter.from_dict(kwds)
+        #self._printer = PortPrinter.from_dict(kwds)
+        self._run_dir = kwds.pop('run_dir', '.')
+        self._kwds = kwds
 
-    def initialize(self):
-        self._printer.open()
+    def initialize(self, *args):
+        with open_run_dir(self._run_dir):
+            self._printer = PortPrinter.from_dict(self._kwds)
+            self._printer.open()
 
     def run(self, time):
-        self._printer.write()
+        with open_run_dir(self._run_dir):
+            self._printer.write()
 
     def finalize(self):
-        self._printer.close()
+        with open_run_dir(self._run_dir):
+            self._printer.close()
