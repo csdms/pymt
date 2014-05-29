73ced788d3ce870ad8f7874bb5c64049e97ee5eb
mcflugen <huttone@colorado.edu>
2014-05-29 11:27:46 -0600

    Changed theme to scipy.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,8 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..', '..')))
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.pardir, 'sphinxext')))
 
 # -- General configuration -----------------------------------------------------
 
@@ -30,7 +31,7 @@ sys.path.insert(0, os.path.abspath(
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
               'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath',
               'sphinx.ext.mathjax', 'sphinx.ext.ifconfig',
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode', 'numpydoc', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -99,15 +100,25 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+html_theme = 'scipy'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+    'edit_link': False,
+    'sidebar': right,
+    'scipy_org_logo': False,
+    'rootlinks': [],
+    'navigation_links': True,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [
+    os.path.join(os.pardir, 'scipy-sphinx-theme', '_theme'),
+]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
