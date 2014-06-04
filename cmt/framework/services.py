2b24f5f015a05f57811f4a62e0a4c6f6f9d774d7
mcflugen <mcflugen@gmail.com>
2014-06-03 18:22:28 -0600

    Added docstrings and doctests.

--- cmt/framework/services.py
+++ cmt/framework/services.py
@@ -1,3 +1,5 @@
+"""Access to framework services.
+"""
 import sys
 import imp
 import warnings
@@ -31,11 +33,17 @@ def set_services(name, path=None, ifset='pass'):
 
 
 def register_component_classes(components):
-    #try:
-    #    components = components.items()
-    #except AttributeError:
-    #    pass
+    """Register a list of components with the framework.
 
+    Parameters
+    ----------
+    components : list
+        Component names to register.
+
+    See Also
+    --------
+    :func:`register_component_class` : Register just a single component.
+    """
     for name in components:
         register_component_class(name)
 
@@ -48,6 +56,45 @@ def _register_component_class(name, cls):
 
 
 def register_component_class(name):
+    """Register a component with the framework.
+
+    Add the component class, *name*, to the list of framework services. The component name should be the
+    fully-qualified name of a Python class.
+
+    Parameters
+    ----------
+    name : str
+        Name of component to register.
+
+    Raises
+    ------
+    ValueError
+        If the class is already registered with the framework.
+    ImportError
+        If there is a problem importing any part of *name*.
+
+    Notes
+    -----
+    This function will try to import *name* as though it were a fully-qualified name of a Python class. That is,
+    if *name* were `foo.bar.Baz`, try the following::
+
+        from foo.bar import Baz
+
+    Examples
+    --------
+    >>> register_component_class('cmt.testing.services.AirPort')
+    >>> get_component_class('AirPort')
+    <class 'services.AirPort'>
+
+    Raise an ImportError if the component class could not be loaded.
+
+    >>> register_component_class('cmt.testing.services.NotAClass') # doctest : +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    ImportError: cannot import name NotAClass from cmt.testing.services
+    >>> register_component_class('cmt.not.a.module.AirPort') # doctest : +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    ImportError: No module named not
+    """
     import sys
 
     parts = name.split('.')
@@ -61,7 +108,6 @@ def register_component_class(name):
         except AttributeError:
             pass
             #raise
-    #register_component_class(cls_name, mod.__dict__[cls_name])
 
     if cls_name in _COMPONENT_CLASSES:
         raise ValueError(cls_name)
@@ -69,10 +115,32 @@ def register_component_class(name):
         try:
             _COMPONENT_CLASSES[cls_name] = mod.__dict__[cls_name]
         except KeyError:
-            raise
+            raise ImportError('cannot import name %s from %s' % (cls_name, '.'.join(parts[:-1])))
 
 
 def register_component_instance(name, instance):
+    """Register a component instance with the framework.
+
+    Parameters
+    ----------
+    name : str
+        Name of the component instance.
+    instance : object
+        Component instance.
+
+    Raises
+    ------
+    ValueError
+        If *name* is already registered with the framework.
+
+    Examples
+    --------
+    >>> from cmt.testing.services import AirPort
+    >>> air_port = AirPort()
+    >>> register_component_instance('air_port', air_port)
+    >>> air_port is get_component_instance('air_port')
+    True
+    """
     if name in _COMPONENT_INSTANCES:
         raise ValueError(name)
     else:
@@ -80,13 +148,77 @@ def register_component_instance(name, instance):
 
 
 def get_component_class(name):
+    """Get a registered component class by name.
+
+    Parameters
+    ----------
+    name : str
+        Name of the registered class.
+
+    Returns
+    -------
+    class
+        The registered class.
+
+    Raises
+    ------
+    KeyError
+        If the *name* is not registered with the framework.
+
+    See Also
+    --------
+    :func:`register_component_class`
+    """
     return _COMPONENT_CLASSES[name]
 
 
 def instantiate_component(cls_name, instance_name):
+    """Instantiate a registered component class.
+
+    Instantiate a registered class and register that instance with the framework as *instance_name*.
+
+    Parameters
+    ----------
+    cls_name : str
+        Name of the registered class.
+    instance_name : str
+        Name of the new instance.
+
+    Returns
+    -------
+    object
+        An instance of the registered class.
+
+    Examples
+    --------
+    >>> register_component_class('cmt.testing.services.AirPort')
+    >>> air_port = instantiate_component('AirPort', 'air_port')
+    >>> air_port is get_component_instance('air_port')
+    """
     register_component_instance(instance_name, get_component_class(cls_name)())
     return get_component_instance(instance_name)
 
 
 def get_component_instance(name):
+    """Get a registered instance from the framework.
+
+    Parameters
+    ----------
+    name : str
+        Name of the registered instance.
+
+    Returns
+    -------
+    object
+        The registered instance.
+
+    Raises
+    ------
+    KeyError
+        If *name* is not registered with the framework.
+
+    See Also
+    --------
+    :func:`instantiate_component`
+    """
     return _COMPONENT_INSTANCES[name]
