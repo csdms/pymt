1ceaa5fea79213676bf0524015e29845ebc9a465
mcflugen <mcflugen@gmail.com>
2014-06-09 16:23:06 -0600

    Fixed failing unit tests.
    Fixed
    ----
    * Fixed unit tests for TimeLine class to reflect the change to
      FIFO popping of events.

--- cmt/tests/test_timeline.py
+++ cmt/tests/test_timeline.py
@@ -38,7 +38,7 @@ def test_non_zero_start():
     timeline = Timeline([('event 1', 1.), ('event 2', .25)], start=-1)
     assert_equal(timeline.time_of_next_event, -.75)
     assert_list_equal(timeline.pop_until(0.),
-                      ['event 2', 'event 2', 'event 2', 'event 2', 'event 1'])
+                      ['event 2', 'event 2', 'event 2', 'event 1', 'event 2'])
 
 
 def test_pop_empty_timeline():
@@ -49,10 +49,13 @@ def test_pop_empty_timeline():
 
 def test_pop_events():
     timeline = Timeline([('event 1', 1.), ('event 2', .25)])
-    for time in [.25, .5, .75, 1.]:
+    for time in [.25, .5, .75]:
         assert_equal('event 2', timeline.pop())
         assert_equal(time, timeline.time)
     assert_equal('event 1', timeline.pop())
+    assert_equal(1., timeline.time)
+
+    assert_equal('event 2', timeline.pop())
     assert_equal(1., timeline.time)
 
 
@@ -63,23 +66,23 @@ def test_pop_until():
     assert_equal(.2, timeline.time)
 
     events = timeline.pop_until(1.)
-    assert_list_equal(['event 2', 'event 2', 'event 1'], events)
+    assert_list_equal(['event 2', 'event 1', 'event 2'], events)
     assert_equal(1., timeline.time)
 
 
 def test_hashable_event():
     timeline = Timeline([(('event', 2), .5), (('event', 0), 1.)])
     assert_tuple_equal(('event', 2), timeline.pop())
-    assert_tuple_equal(('event', 2), timeline.pop())
     assert_tuple_equal(('event', 0), timeline.pop())
+    assert_tuple_equal(('event', 2), timeline.pop())
 
 
 def test_unhashable_event():
     (first_event, second_event) = (dict(foo='bar'), dict(bar='baz'))
     timeline = Timeline([(first_event, .5), (second_event, 1.)])
     assert_is(first_event, timeline.pop())
-    assert_is(first_event, timeline.pop())
     assert_is(second_event, timeline.pop())
+    assert_is(first_event, timeline.pop())
 
 
 def test_one_time_event():
@@ -96,17 +99,17 @@ def test_repeated_events():
                          (first_event, .75)])
     assert_is(first_event, timeline.pop())
     assert_is(first_event, timeline.pop())
-    assert_is(first_event, timeline.pop())
     assert_is(second_event, timeline.pop())
+    assert_is(first_event, timeline.pop())
 
     timeline = Timeline([(first_event, .5),
                          (second_event, 1.),
                          (first_event, 1.),
                         ])
     assert_is(first_event, timeline.pop())
-    assert_is(first_event, timeline.pop())
-    assert_is(first_event, timeline.pop())
     assert_is(second_event, timeline.pop())
+    assert_is(first_event, timeline.pop())
+    assert_is(first_event, timeline.pop())
 
     timeline = Timeline([(first_event, .5),
                          (first_event, 1.),
