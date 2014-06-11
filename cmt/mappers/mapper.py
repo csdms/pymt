5105ce25c3a2645d4ac89a8d7b4ed07cdccad83e
mcflugen <mcflugen@gmail.com>
2014-06-11 17:24:47 -0600

    Changed for PEP8 styling.

--- cmt/mappers/mapper.py
+++ cmt/mappers/mapper.py
@@ -12,18 +12,18 @@ Point-to-point Mapping
 >>> src = Rectilinear([0, 1, 2], [0, 2])
 >>> dst = Rectilinear([.5, 1.5, 2.5], [.25, 1.25])
 
->>> src.get_x ()
+>>> src.get_x()
 array([ 0.,  2.,  0.,  2.,  0.,  2.])
 
->>> src.get_y ()
+>>> src.get_y()
 array([ 0.,  0.,  1.,  1.,  2.,  2.])
 
->>> dst.get_x ()
+>>> dst.get_x()
 array([ 0.25,  1.25,  0.25,  1.25,  0.25,  1.25])
->>> dst.get_y ()
+>>> dst.get_y()
 array([ 0.5,  0.5,  1.5,  1.5,  2.5,  2.5])
 
->>> src_vals = np.arange(src.get_point_count(), dtype=float)
+>>> src_vals = np.arange(src.get_point_count(), dtype=np.float64)
 
 Map the source values on the source points to the destination grid
 using nearest neighbor.
@@ -35,16 +35,15 @@ using nearest neighbor.
 >>> mapper.run(src_vals)
 array([ 0.,  1.,  2.,  3.,  4.,  5.])
 
->>> mappers = find_mapper (dst, src)
->>> len (mappers)
+>>> mappers = find_mapper(dst, src)
+>>> len(mappers)
 3
->>> mappers[0].name ()
+>>> mappers[0].name()
 'PointToPoint'
 
 >>> src_vals[2] = -999
->>> dst_vals = np.zeros (dst.get_point_count ())-1
->>> dummy = mapper.run (src_vals, dst_vals)
->>> dst_vals
+>>> dst_vals = np.zeros(dst.get_point_count ()) - 1
+>>> mapper.run(src_vals, dst_vals)
 array([ 0.,  1., -1.,  3.,  4.,  5.])
 
 Cell-to-point Mapping
@@ -69,22 +68,22 @@ The source grid looks like,
 >>> from cmt.grids.map import UniformRectilinearMap as UniformRectilinear
 >>> from cmt.grids.map import UnstructuredPointsMap as UnstructuredPoints
 
->>> (dst_x, dst_y) = (np.array ([.45, 1.25, 3.5]), np.array ([.75, 2.25, 3.25]))
+>>> (dst_x, dst_y) = (np.array([.45, 1.25, 3.5]), np.array([.75, 2.25, 3.25]))
 
->>> src = UniformRectilinear ((2,4), (2, 1), (0, 0))
->>> dst = UnstructuredPoints (dst_x, dst_y)
+>>> src = UniformRectilinear((2,4), (2, 1), (0, 0))
+>>> dst = UnstructuredPoints(dst_x, dst_y)
 
->>> src_vals = np.arange (src.get_cell_count (), dtype=np.float)
+>>> src_vals = np.arange(src.get_cell_count(), dtype=np.float64)
 
->>> mapper = CellToPoint ()
->>> mapper.initialize (dst, src)
->>> mapper.run (src_vals, bad_val=-999)
+>>> mapper = CellToPoint()
+>>> mapper.initialize(dst, src)
+>>> mapper.run(src_vals, bad_val=-999)
 array([   0.,    2., -999.])
 
->>> src_vals = np.arange (src.get_cell_count (), dtype=np.float)
+>>> src_vals = np.arange(src.get_cell_count(), dtype=np.float64)
 >>> src_vals[0] = -9999
->>> dst_vals = np.zeros (dst.get_point_count ())+100
->>> _ = mapper.run (src_vals, dst_vals)
+>>> dst_vals = np.zeros(dst.get_point_count()) + 100
+>>> _ = mapper.run(src_vals, dst_vals)
 >>> dst_vals
 array([ 100.,    2., -999.])
 
@@ -92,25 +91,26 @@ Point-to-cell Mapping
 ---------------------
 
 >>> from cmt.mappers import PointToCell
->>> (src_x, src_y) = (np.array ([.45, 1.25, 3.5, .0, 1.]), np.array ([.75, 2.25, 3.25, .9, 1.1]))
+>>> (src_x, src_y) = (np.array ([.45, 1.25, 3.5, .0, 1.]),
+...                   np.array ([.75, 2.25, 3.25, .9, 1.1]))
 
->>> src = UnstructuredPoints (src_x, src_y)
->>> dst = UniformRectilinear ((2,4), (2, 1), (0, 0))
+>>> src = UnstructuredPoints(src_x, src_y)
+>>> dst = UniformRectilinear((2,4), (2, 1), (0, 0))
 
->>> src_vals = np.arange (src.get_point_count (), dtype=np.float)
+>>> src_vals = np.arange(src.get_point_count(), dtype=np.float64)
 
->>> mapper = PointToCell ()
->>> mapper.initialize (dst, src)
+>>> mapper = PointToCell()
+>>> mapper.initialize(dst, src)
 
->>> mapper.run (src_vals, bad_val=-999)
+>>> mapper.run(src_vals, bad_val=-999)
 array([ 1.5,  4. ,  1. ])
 
->>> mapper.run (src_vals, bad_val=-999, method=np.sum)
+>>> mapper.run(src_vals, bad_val=-999, method=np.sum)
 array([ 3.,  4.,  1.])
 
 >>> src_vals[0] = -9999
->>> dst_vals = np.zeros (dst.get_cell_count ())-1
->>> _ = mapper.run (src_vals, dst_vals)
+>>> dst_vals = np.zeros(dst.get_cell_count ()) - 1
+>>> _ = mapper.run(src_vals, dst_vals)
 >>> dst_vals
 array([-1.,  4.,  1.])
 
@@ -118,15 +118,16 @@ array([-1.,  4.,  1.])
 Point on cell edges
 -------------------
 
->>> (src_x, src_y) = (np.array ([0, .5, 1., 2, 3.5]), np.array ([1., 1., .0, 3, 3.]))
->>> src = UnstructuredPoints (src_x, src_y)
->>> dst = UniformRectilinear ((2,4), (2, 1), (0, 0))
+>>> (src_x, src_y) = (np.array ([0, .5, 1., 2, 3.5]),
+...                   np.array ([1., 1., .0, 3, 3.]))
+>>> src = UnstructuredPoints(src_x, src_y)
+>>> dst = UniformRectilinear((2,4), (2, 1), (0, 0))
 
->>> mapper = PointToCell ()
->>> mapper.initialize (dst, src)
+>>> mapper = PointToCell()
+>>> mapper.initialize(dst, src)
 
->>> src_vals = np.arange(src.get_point_count (), dtype=np.float)
->>> dst_vals = np.zeros(dst.get_cell_count ()) - 1
+>>> src_vals = np.arange(src.get_point_count(), dtype=np.float64)
+>>> dst_vals = np.zeros(dst.get_cell_count()) - 1
 >>> _ = mapper.run(src_vals, dst_vals)
 >>> dst_vals
 array([ 1. ,  0.5,  3. ])
@@ -142,17 +143,18 @@ A big mapper
 >>> mapper = PointToCell()
 >>> mapper.initialize(dst, src)
 
->>> src_vals = np.arange(src.get_point_count(), dtype=np.float)
->>> dst_vals = np.zeros(dst.get_cell_count(), dtype=np.float) - 1
+>>> src_vals = np.arange(src.get_point_count(), dtype=np.float64)
+>>> dst_vals = np.zeros(dst.get_cell_count(), dtype=np.float64) - 1
 >>> _ = mapper.run(src_vals, dst_vals)
 
->>> all (dst_vals==src_vals)
-True
+>>> from numpy.testing import assert_array_equal
+>>> assert_array_equal(dst_vals, src_vals)
 """
 
 from pointtopoint import NearestVal
 from celltopoint import CellToPoint
 from pointtocell import PointToCell
+
 
 class Error(Exception):
     pass
