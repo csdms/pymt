c1571dabed346ba9ede8bf43db13e93e42129dd1
mcflugen <mcflugen@gmail.com>
2014-06-11 15:49:25 -0600

    Changed to PEP8 styling.

--- cmt/printers/bov/database.py
+++ cmt/printers/bov/database.py
@@ -2,23 +2,24 @@
 
 import os
 
-import numpy as np
-
 from .bov import tofile
 
 
-class IDatabase (object):
-    def __init__ (self):
+class IDatabase(object):
+    def __init__(self):
         pass
-    def open (self, path, var_name):
+
+    def open(self, path, var_name):
         pass
-    def write (self, field):
+
+    def write(self, field):
         pass
-    def close (self):
+
+    def close(self):
         pass
 
 
-class Database (IDatabase):
+class Database(IDatabase):
     """
     >>> import numpy as np
     >>> from cmt.grids import RasterField
@@ -49,31 +50,37 @@ append the variable name to the file name.
     True
 
     >>> db.close ()
-
     """
-    def open (self, path, var_name, **kwargs):
-        self.close ()
+    def __init__(self):
+        super(Database, self).__init__()
 
-        (root, ext) = os.path.splitext (path)
+        self._var_name = ''
+        self._path = ''
+        self._template = ''
+        self._options = {}
+
+    def open(self, path, var_name, **kwargs):
+        self.close()
+
+        (root, ext) = os.path.splitext(path)
 
         self._var_name = var_name
         self._path = path
         self._template = '%s_%%04d%s' % (root, ext)
 
-        self._options = {}
-        self._options.update (**kwargs)
+        self._options.update(**kwargs)
 
-    def write (self, field):
-        file_name = self._next_file_name ()
+    def write(self, field):
+        file_name = self._next_file_name()
         tofile(file_name, field, no_clobber=False, options=self._options)
 
-    def close (self):
+    def close(self):
         try:
             del self._count
         except AttributeError:
             pass
 
-    def _next_file_name (self):
+    def _next_file_name(self):
         try:
             next_file_name = self._template % self._count
         except AttributeError:
@@ -83,8 +90,3 @@ append the variable name to the file name.
             self._count += 1
 
         return next_file_name
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod() 
-
