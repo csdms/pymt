cec3bbb6859d9400eaf0fc8940577a2a96950c2e
mcflugen <mcflugen@gmail.com>
2014-06-16 15:32:03 -0600

    Changed to install ESMF in ~/_inst.

--- .travis/install_esmf.sh
+++ .travis/install_esmf.sh
@@ -6,5 +6,6 @@ cd $BASE_DIR
 git archive --remote=git://git.code.sf.net/p/esmf/esmf --format=tar --prefix=esmf/ ESMF_6_3_0r | tar xf -
 export ESMF_DIR=$BASE_DIR/esmf
 export ESMF_COMPILER=gfortran
+export ESMF_INSTALL_PREFIX=$BASE_DIR/_inst
 cd $ESMF_DIR && make
-sudo make install
+make install
--- .travis/install_esmpy.sh
+++ .travis/install_esmpy.sh
@@ -2,10 +2,11 @@
 
 OS=Linux
 BASE_DIR=$HOME
+cd $BASE_DIR
 
 git archive --remote=git://git.code.sf.net/p/esmfcontrib/ESMPy --format=tar --prefix=ESMPy/ 630r_01b | tar xf -
 export ESMF_DIR=$BASE_DIR/esmf
-export ESMFMKFILE=$ESMF_DIR/_inst/lib/libO/$OS.gfortran.64.mpiuni.default/esmf.mk
+export ESMFMKFILE=$BASE_DIR/_inst/lib/libO/$OS.gfortran.64.mpiuni.default/esmf.mk
 
 cd ESMPy
 sudo python setup.py build install
