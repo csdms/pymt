453c3a414e44ddc35ef850e2582846fd76e6ffaa
mcflugen <mcflugen@gmail.com>
2014-06-18 11:19:34 -0600

    Removed mappers subpackage imports.

--- cmt/mappers/__init__.py
+++ cmt/mappers/__init__.py
@@ -1,11 +1,1 @@
 
-from imapper import *
-from mapper import *
-from pointtopoint import *
-from pointtocell import *
-from celltopoint import *
-try:
-    from esmp import *
-except ImportError:
-    import warnings
-    warnings.warn ('Unable to import ESMP', RuntimeWarning)
