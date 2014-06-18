f8f6d4690882500ac190ef9fdb864be7c644e9c2
mcflugen <mcflugen@gmail.com>
2014-06-18 16:51:57 -0600

    Changed to delete services before test setup.

--- cmt/component/tests/__init__.py
+++ cmt/component/tests/__init__.py
@@ -1,7 +1,9 @@
 def setup():
     from cmt.framework.services import (register_component_classes,
-                                        instantiate_component)
+                                        del_services)
 
+
+    del_services()
 
     register_component_classes(["cmt.testing.services.AirPort",
                                 "cmt.testing.services.EarthPort"])
