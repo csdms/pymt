4443f6d2493ce6ff9d49cf4049c6a89d2a61eddf
mcflugen <mcflugen@gmail.com>
2014-06-20 13:41:35 -0600

    Read test data files from a remote URL.
    Changed
    -------
    * Instead of reading test netcdf files from a data directory in the repository, fetch them from a URL.

--- cmt/printers/nc/tests/test_ugrid_read.py
+++ cmt/printers/nc/tests/test_ugrid_read.py
@@ -1,7 +1,18 @@
 import unittest
 import os
+import urllib2
 
 from cmt.printers.nc.read import field_fromfile
+
+
+def fetch_data_file(filename):
+    url = ('http://csdms.colorado.edu/thredds/fileServer/benchmark/ugrid/' +
+           filename)
+    remote_file = urllib2.urlopen(url)
+    with open(filename, 'w') as netcdf_file:
+        netcdf_file.write(remote_file.read())
+
+    return os.path.abspath(filename)
 
 
 class DataTestFileMixIn(object):
@@ -14,17 +25,17 @@ class DataTestFileMixIn(object):
 
 class TestNetcdfRead(unittest.TestCase, DataTestFileMixIn):
     def test_unstructured_2d(self):
-        field = field_fromfile(self.data_file('unstructured.2d.nc'),
+        field = field_fromfile(fetch_data_file('unstructured.2d.nc'),
                                format='NETCDF4')
 
     def test_rectilinear_1d(self):
-        field = field_fromfile(self.data_file('rectilinear.1d.nc'),
+        field = field_fromfile(fetch_data_file('rectilinear.1d.nc'),
                                format='NETCDF4')
 
     def test_rectilinear_2d(self):
-        field = field_fromfile(self.data_file('rectilinear.2d.nc'),
+        field = field_fromfile(fetch_data_file('rectilinear.2d.nc'),
                                format='NETCDF4')
 
     def test_rectilinear_3d(self):
-        field = field_fromfile(self.data_file('rectilinear.3d.nc'),
+        field = field_fromfile(fetch_data_file('rectilinear.3d.nc'),
                                format='NETCDF4')
