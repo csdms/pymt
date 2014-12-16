c3272994625bdb066a486abee9e092dc79b894a4
mcflugen <mcflugen@gmail.com>
2014-12-16 15:28:01 -0700

    PEP8 clean up.

--- cmt/printers/vtk/vtk.py
+++ cmt/printers/vtk/vtk.py
@@ -12,112 +12,116 @@ Define a grid that consists of two trianges that share two points.
     (0) - (1)
 
 Create the grid,
-    >>> from cmt.printers.vtk import tofile
 
-    >>> from cmt.grids.field import GridField
-    >>> g = GridField ([0, 2, 1, 3], [0, 0, 1, 1], [0, 2, 1, 2, 3, 1], [3, 6])
-    >>> g.add_field ('Elevation',  [1., 2., 3, 4], centering='point')
-    >>> g.add_field ('Temperature',  [10., 20., 30., 40.], centering='point')
-    >>> g.add_field ('Cell Elevation', [1., 2.], centering='zonal')
-    >>> g.add_field ('Cell Temperature', [10., 20.], centering='zonal')
-    >>> tofile (g, 'tri.vtu', format='appended', encoding='base64')
+>>> from cmt.printers.vtk import tofile
 
-
+>>> from cmt.grids.field import GridField
+>>> g = GridField ([0, 2, 1, 3], [0, 0, 1, 1], [0, 2, 1, 2, 3, 1], [3, 6])
+>>> g.add_field ('Elevation',  [1., 2., 3, 4], centering='point')
+>>> g.add_field ('Temperature',  [10., 20., 30., 40.], centering='point')
+>>> g.add_field ('Cell Elevation', [1., 2.], centering='zonal')
+>>> g.add_field ('Cell Temperature', [10., 20.], centering='zonal')
+>>> tofile (g, 'tri.vtu', format='appended', encoding='base64')
 """
 import os
-import sys
 import xml.dom.minidom
-
-import numpy as np
-
-from .encoders import encode
 
 
 valid_encodings = ['ascii', 'base64', 'raw']
 valid_formats = ['ascii', 'base64', 'raw', 'appended']
 
-class VtkError (Exception):
+
+class VtkError(Exception):
     pass
-class InvalidFormatError (VtkError):
-    def __init__ (self, format):
-        self.name = format
-    def __str__ (self):
+
+
+class InvalidFormatError(VtkError):
+    def __init__(self, fmt):
+        self.name = fmt
+
+    def __str__(self):
         return '%s: Invalid format' % self.name
-class InvalidEncodingError (VtkError):
-    def __init__ (self, encoding):
+
+
+class InvalidEncodingError(VtkError):
+    def __init__(self, encoding):
         self.name = encoding
-    def __str__ (self):
+    def __str__(self):
         return '%s: Invalid encoder' % self.name
 
-class VtkWriter (xml.dom.minidom.Document):
 
-  def __init__ (self, *args, **kwargs):
-    xml.dom.minidom.Document.__init__ (self)
+class VtkWriter(xml.dom.minidom.Document):
+    def __init__(self, *args, **kwargs):
+        xml.dom.minidom.Document.__init__(self)
 
-  def assemble (self, element):
-      root = element['VTKFile']
-      grid = element['Grid']
-      piece = element['Piece']
-      for section in ['Points', 'Coordinates', 'PointData', 'Cells', 'CellData']:
-          try:
-              piece.appendChild (element[section])
-          except KeyError:
-              pass
-          except EmptyElementError:
-              pass
-      grid.appendChild (piece)
-      root.appendChild (grid)
+    def assemble(self, element):
+        root = element['VTKFile']
+        grid = element['Grid']
+        piece = element['Piece']
+        for section in ['Points', 'Coordinates', 'PointData', 'Cells',
+                        'CellData']:
+            try:
+                piece.appendChild(element[section])
+            except KeyError:
+                pass
+            except EmptyElementError:
+                pass
+        grid.appendChild(piece)
+        root.appendChild(grid)
+  
+        try:
+            root.appendChild(element['AppendedData'])
+        except KeyError:
+            pass
+  
+        return root
+  
+    def write(self, path, format='ascii', encoding='ascii'):
+        if format not in valid_formats:
+            raise InvalidFormatError(format)
+        if encoding not in valid_encodings:
+            raise InvalidEncodingError(encoding)
+  
+        if format == 'ascii':
+            encoding = 'ascii'
+  
+        self.unlink()
+        self.appendChild(self.assemble(self.get_elements(format=format,
+                                                         encoding=encoding)))
+        self.to_xml(path)
+  
+    def to_xml(self, path):
+        file = open(path, 'w')
+        file.write(self.toprettyxml())
+        file.close()
 
-      try:
-          root.appendChild (element['AppendedData'])
-      except KeyError:
-          pass
 
-      return root
-
-  def write (self, path, format='ascii', encoding='ascii'):
-      if format not in valid_formats:
-          raise InvalidFormatError (format)
-      if encoding not in valid_encodings:
-          raise InvalidEncodingError (encoding)
-
-      if format=='ascii':
-          encoding = 'ascii'
-
-      self.unlink ()
-      self.appendChild (self.assemble (self.get_elements (format=format, encoding=encoding)))
-      self.to_xml (path)
-
-  def to_xml (self, path):
-      file = open (path, 'w')
-      file.write (self.toprettyxml ())
-      file.close ()
-
-def assemble_vtk_elements (element):
+def assemble_vtk_elements(element):
     root = element['VTKFile']
     grid = element['Grid']
     piece = element['Piece']
     for section in ['Points', 'Coordinates', 'PointData', 'Cells', 'CellData']:
         try:
-            piece.appendChild (element[section])
+            piece.appendChild(element[section])
         except KeyError:
             pass
         except EmptyElementError:
             pass
-    grid.appendChild (piece)
-    root.appendChild (grid)
+    grid.appendChild(piece)
+    root.appendChild(grid)
 
     try:
-        root.appendChild (element['AppendedData'])
+        root.appendChild(element['AppendedData'])
     except KeyError:
         pass
 
     return root
 
-class VTKDatabase (VtkWriter):
-    def write (self, path, **kwargs):
-        (base, file) = os.path.split (path)
-        (root, ext) = os.path.splitext (file)
+
+class VTKDatabase(VtkWriter):
+    def write(self, path, **kwargs):
+        (base, file) = os.path.split(path)
+        (root, ext) = os.path.splitext(file)
 
         try:
             next_file = '%s_%04d%s' % (root, self._count, ext)
@@ -125,11 +129,6 @@ class VTKDatabase (VtkWriter):
             self._count = 0
             next_file = '%s_%04d%s' % (root, self._count, ext)
 
-        VtkWriter.write (self, os.path.join (base, next_file), **kwargs)
+        VtkWriter.write(self, os.path.join (base, next_file), **kwargs)
 
         self._count += 1
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-
