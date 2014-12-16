6799e7fcbbe4df82e3d2bc28ef7865019d4e79e8
mcflugen <mcflugen@gmail.com>
2014-12-16 15:15:55 -0700

    Use __all__ for package imports.

--- cmt/grids/__init__.py
+++ cmt/grids/__init__.py
@@ -9,3 +9,11 @@ from .assertions import (is_uniform_rectilinear, is_rectilinear, is_structured,
                          is_unstructured)
 
 from .igrid import DimensionError
+
+__all__ = ['UniformRectilinear', 'UniformRectilinearPoints', 'Rectilinear',
+           'RectilinearPoints', 'Structured', 'StructuredPoints',
+           'Unstructured', 'UnstructuredPoints', 'RasterField',
+           'RectilinearField', 'StructuredField', 'UnstructuredField',
+           'is_uniform_rectilinear', 'is_rectilinear', 'is_structured',
+           'is_unstructured', 'DimensionError']
+
--- cmt/mappers/__init__.py
+++ cmt/mappers/__init__.py
@@ -1,5 +1,7 @@
-
 from .mapper import find_mapper, IncompatibleGridError
 from .celltopoint import CellToPoint
 from .pointtopoint import NearestVal
 from .pointtocell import PointToCell
+
+__all__ = ['find_mapper', 'IncompatibleGridError', 'CellToPoint',
+           'NearestVal', 'PointToCell']
