aa711dbb07de1db134f4567eba929ada01d70e1a
mcflugen <mcflugen@gmail.com>
2014-12-16 16:00:35 -0700

    Make keywords immutable.

--- cmt/printers/nc/write.py
+++ cmt/printers/nc/write.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python
 
-from ...grids.assertions import (is_rectilinear, is_structured,
-                                 is_unstructured)
+from ...grids.assertions import (is_rectilinear, is_structured)
 from .ugrid import (NetcdfRectilinearField, NetcdfStructuredField,
                     NetcdfUnstructuredField)
 
 
-def field_tofile(field, path, append=False, attrs={}, time=None,
+def field_tofile(field, path, append=False, attrs=None, time=None,
                  time_units='days', time_reference='00:00:00 UTC',
-                 long_name={}, format='NETCDF4', keep_open=False):
-
+                 long_name=None, format='NETCDF4', keep_open=False):
+    attrs = attrs or {}
+    long_name = long_name or {}
     args = (path, field)
     kwds = dict(append=append, format=format, time=time, keep_open=True)
 
