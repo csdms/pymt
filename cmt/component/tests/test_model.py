b0d38f3d8c115a0719b07a993ead6198855a1a4b
mcflugen <mcflugen@gmail.com>
2014-12-20 14:57:15 -0700

    Added unit tests for Model class.

---
+++ cmt/component/tests/test_model.py
@@ -0,0 +1,51 @@
+import os
+
+from nose.tools import assert_equal
+
+from cmt.component.model import Model
+from cmt.testing.assertions import assert_isfile_and_remove
+from cmt.framework.services import del_component_instances
+from cmt.utils.run_dir import cd_temp
+
+
+AIR_PORT_CONTENTS = """
+name: air_port
+class: AirPort
+argv: []
+initialize_args: air.txt
+print: []
+run_dir: air
+time_step: 1.0
+connectivity: []
+"""
+
+def test_model_load():
+    del_component_instances(['air_port'])
+    with cd_temp() as _:
+        os.mkdir('air')
+        model = Model.load(AIR_PORT_CONTENTS)
+
+        assert_equal(model.components, ['air_port'])
+
+
+def test_model_from_file():
+    del_component_instances(['air_port'])
+    with cd_temp() as _:
+        os.mkdir('air')
+        with open('components.yml', 'w') as fp:
+            fp.write(AIR_PORT_CONTENTS)
+        model = Model.from_file('components.yml')
+
+        assert_equal(model.components, ['air_port'])
+
+
+def test_model_from_file_like():
+    del_component_instances(['air_port'])
+    with cd_temp() as _:
+        os.mkdir('air')
+        with open('components.yml', 'w') as fp:
+            fp.write(AIR_PORT_CONTENTS)
+        with open('components.yml', 'r') as fp:
+            model = Model.from_file_like(fp)
+
+        assert_equal(model.components, ['air_port'])
