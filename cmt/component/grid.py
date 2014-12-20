34fdd068e23c28cf75fe6af7cfbc1f18286c8a88
mcflugen <mcflugen@gmail.com>
2014-12-20 00:20:50 -0700

    Added more doctests.

--- cmt/component/grid.py
+++ cmt/component/grid.py
@@ -63,48 +63,193 @@ def get_raster_node_coordinates(grid, name):
 
     Examples
     --------
+    >>> from cmt.component.grid import get_raster_node_coordinates
+    >>> class RasterGrid(object):
+    ...     def get_grid_shape(self, name):
+    ...         return (2, 3)
+    ...     def get_grid_spacing(self, name):
+    ...         return (1., 2.)
+    ...     def get_grid_origin(self, name):
+    ...         return (2., 1.)
+    >>> g = RasterGrid()
+    >>> (y, x) = get_raster_node_coordinates(g, 'var')
+    >>> y
+    array([[ 2.,  2.,  2.],
+           [ 3.,  3.,  3.]])
+    >>> x
+    array([[ 1.,  3.,  5.],
+           [ 1.,  3.,  5.]])
     """
     (shape, spacing, origin) = (grid.get_grid_shape(name),
                                 grid.get_grid_spacing(name),
                                 grid.get_grid_origin(name))
 
-    coordinate_vectors = []
-    for dim in xrange(len(shape)):
-        coordinate_vectors.append(np.arange(
-            origin[dim], origin[dim] + shape[dim] * spacing[dim], spacing[dim]))
-    
-    return np.meshgrid(*coordinate_vectors, indexing='ij')
+    return raster_node_coordinates(shape, spacing=spacing, origin=origin)
 
 
 def get_rectilinear_node_coordinates(grid, name):
+    """Get coordinates of nodes on a rectilinear grid.
+
+    Parameters
+    ----------
+    grid: grid_like
+        A rectilinear grid.
+    name : str
+        Name of the field variable.
+
+    Returns
+    -------
+    coords : ndarray
+        Coordinates of the grid nodes.
+
+    Examples
+    --------
+    >>> from cmt.component.grid import get_rectilinear_node_coordinates
+    >>> class RectilinearGrid(object):
+    ...     def get_grid_x(self, name):
+    ...         return (0., 3., 4)
+    ...     def get_grid_y(self, name):
+    ...         return (2., 7.)
+    >>> g = RectilinearGrid()
+    >>> (y, x) = get_rectilinear_node_coordinates(g, 'var')
+    >>> y
+    array([[ 2.,  2.,  2.],
+           [ 7.,  7.,  7.]])
+    >>> x
+    array([[ 0.,  3.,  4.],
+           [ 0.,  3.,  4.]])
+    """
     coordinate_vectors = []
-    for coordinate_name in ['z', 'y', 'z']:
-        func = getattr(grid, 'get_grid_' + coordinate_name)
+    for coordinate_name in ['z', 'y', 'x']:
         try:
+            func = getattr(grid, 'get_grid_' + coordinate_name)
             coordinate_vectors.append(func(name))
-        except Exception:
+        except (AttributeError, NotImplementedError):
             pass
 
     return np.meshgrid(*coordinate_vectors, indexing='ij')
 
 
 def get_structured_node_coordinates(grid, name):
+    """Get coordinates of nodes on a structured grid.
+
+    Parameters
+    ----------
+    grid: grid_like
+        A structured grid.
+    name : str
+        Name of the field variable.
+
+    Returns
+    -------
+    coords : ndarray
+        Coordinates of the grid nodes.
+
+    Examples
+    --------
+    >>> from cmt.component.grid import get_structured_node_coordinates
+    >>> class StructuredGrid(object):
+    ...     def get_grid_x(self, name):
+    ...         return np.array((0., 3., 4, 0., 3., 4.)).reshape((2, 3))
+    ...     def get_grid_y(self, name):
+    ...         return np.array((2., 2., 2., 7., 7., 7.)).reshape((2, 3))
+    ...     def get_grid_z(self, name):
+    ...         raise NotImplementedError('get_grid_z')
+    >>> g = StructuredGrid()
+    >>> (y, x) = get_structured_node_coordinates(g, 'var')
+    >>> y
+    array([[ 2.,  2.,  2.],
+           [ 7.,  7.,  7.]])
+    >>> x
+    array([[ 0.,  3.,  4.],
+           [ 0.,  3.,  4.]])
+    """
     node_coordinates = []
     for coordinate_name in ['z', 'y', 'x']:
-        func = getattr(grid, 'get_grid_' + coordinate_name)
         try:
-            node_coordinates.append(func(name))
-        except Exception:
+            func = getattr(grid, 'get_grid_' + coordinate_name)
+            node_coordinates.append(np.array(func(name)))
+        except (AttributeError, NotImplementedError):
             pass
     return node_coordinates
 
 
 def get_structured_node_connectivity(grid, name):
+    """Get cell connectivity on a structured grid of quadrilaterals.
+
+    Parameters
+    ----------
+    grid: grid_like
+        A structured grid.
+    name : str
+        Name of the field variable.
+
+    Returns
+    -------
+    conn : ndarray
+        Connectivities of nodes to cells.
+
+    Examples
+    --------
+    >>> from cmt.component.grid import get_structured_node_connectivity
+    >>> class StructuredGrid(object):
+    ...     def get_grid_shape(self, name):
+    ...         return (2, 3)
+    >>> g = StructuredGrid()
+    >>> (c, o) = get_structured_node_connectivity(g, 'var')
+    >>> c
+    array([0, 1, 4, 3, 1, 2, 5, 4])
+    >>> o
+    array([4, 8])
+    """
     shape = grid.get_grid_shape(name)
     return get_connectivity(shape, with_offsets=True)
 
 
 class GridMixIn(object):
+    """Mix-in that makes a Component grid-like.
+
+    Examples
+    --------
+    >>> class Port(object):
+    ...     def get_component_name(self):
+    ...         return 'test-component'
+    ...     def get_input_item_count(self):
+    ...         return 1
+    ...     def get_input_item_list(self):
+    ...         return ['invar']
+    ...     def get_output_item_count(self):
+    ...         return 1
+    ...     def get_output_item_list(self):
+    ...         return ['outvar']
+    ...     def get_grid_shape(self, name):
+    ...         return (2, 3)
+    ...     def get_grid_spacing(self, name):
+    ...         return (2., 1.)
+    ...     def get_grid_origin(self, name):
+    ...         return (0., 0.)
+
+    >>> from cmt.component.grid import GridMixIn
+    >>> class Component(GridMixIn):
+    ...     def __init__(self):
+    ...         self._port = Port()
+    ...         super(Component, self).__init__()
+    >>> c = Component()
+    >>> c.name
+    'test-component'
+    >>> c.input_items
+    ['invar']
+    >>> c.output_items
+    ['outvar']
+    >>> c.get_grid_type('invar')
+    'RASTER'
+    >>> c.get_x('invar')
+    array([[ 0.,  1.,  2.],
+           [ 0.,  1.,  2.]])
+    >>> c.get_y('invar')
+    array([[ 0.,  0.,  0.],
+           [ 2.,  2.,  2.]])
+    """
     def __init__(self):
         self._coords = {}
         self._connectivity = {}
@@ -112,10 +257,14 @@ class GridMixIn(object):
 
     @property
     def name(self):
+        """Name of the wrapped component.
+        """
         return self._port.get_component_name()
 
     @property
     def input_items(self):
+        """Input item names as a list.
+        """
         if self._port.get_input_item_count() > 0:
             return self._port.get_input_item_list()
         else:
@@ -123,6 +272,8 @@ class GridMixIn(object):
 
     @property
     def output_items(self):
+        """Output item names as a list.
+        """
         if self._port.get_output_item_count() > 0:
             return self._port.get_output_item_list()
         else:
@@ -149,6 +300,18 @@ class GridMixIn(object):
         self._grid_type[var_name] = grid_type
 
     def get_grid_type(self, var_name):
+        """The type of the grid.
+
+        Parameters
+        ----------
+        var_name : str
+            Name of the variable on the grid.
+
+        Returns
+        -------
+        type : str
+            The type of the grid.
+        """
         try:
             return self._grid_type[var_name]
         except (AttributeError, KeyError):
@@ -224,4 +387,3 @@ class GridMixIn(object):
 
     def set_grid_values(self, name, values):
         return self._port.set_grid_values(name, values)
-
