b10832834af70cd562151c1361ec10e3126aa84f
mcflugen <mcflugen@gmail.com>
2014-12-20 14:32:10 -0700

    Added cd_temp context manager.

--- cmt/utils/run_dir.py
+++ cmt/utils/run_dir.py
@@ -1,4 +1,36 @@
 import os
+import tempfile
+import shutil
+
+
+class cd(object):
+    def __init__(self, path, create=False):
+        self._dir = os.path.abspath(path)
+        self._starting_dir = os.path.abspath(os.getcwd())
+        self._create = create
+
+    def __enter__(self):
+        if self._create and not os.path.isdir(self._dir):
+            os.makedirs(self._dir)
+        os.chdir(self._dir)
+        return os.path.abspath(os.getcwd())
+
+    def __exit__(self, exception_type, value, traceback):
+        os.chdir(self._starting_dir)
+
+
+class cd_temp(object):
+    def __init__(self, **kwds):
+        self._dir = os.path.abspath(tempfile.mkdtemp(**kwds))
+        self._starting_dir = os.path.abspath(os.getcwd())
+
+    def __enter__(self):
+        os.chdir(self._dir)
+        return os.path.abspath(os.getcwd())
+
+    def __exit__(self, exception_type, value, traceback):
+        os.chdir(self._starting_dir)
+        shutil.rmtree(self._dir)
 
 
 class RunDir(object):
