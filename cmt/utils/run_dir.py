967c4bf7f5278e08ea50a081c7b49f9e2d614d2c
mcflugen <mcflugen@gmail.com>
2014-12-20 14:52:36 -0700

    Added doctest for cd_temp.

--- cmt/utils/run_dir.py
+++ cmt/utils/run_dir.py
@@ -20,6 +20,32 @@ class cd(object):
 
 
 class cd_temp(object):
+    """Create a temporary directory and change to it.
+
+    Parameters
+    ----------
+    kwds : dict (optional)
+        All keywords are passed directory to *tempfile.mkdtemp*.
+
+    Returns
+    -------
+    path : str
+        Absolute path to the newly-created temporary directory.
+
+    Examples
+    --------
+    >>> from cmt.utils.run_dir import cd_temp
+    >>> import os
+    >>> starting_dir = os.getcwd()
+    >>> with cd_temp() as path:
+    ...     temp_dir = os.getcwd()
+    >>> temp_dir == path
+    True
+    >>> temp_dir != starting_dir
+    True
+    >>> os.path.exists(temp_dir)
+    False
+    """
     def __init__(self, **kwds):
         self._dir = os.path.abspath(tempfile.mkdtemp(**kwds))
         self._starting_dir = os.path.abspath(os.getcwd())
