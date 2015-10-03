251ad5226f210d5a70192426cda49541ec3f889c
mcflugen <mcflugen@gmail.com>
2015-10-03 15:22:03 -0600

    Set environment before import babelized components.
    Before trying to import babelized components, set SIDL_DLL_PATH and
    LD_LIBRARY_PATH using csdms-config and cca-spec-babel-config programs.

--- cmt/components.py
+++ cmt/components.py
@@ -1,6 +1,6 @@
 __all__ = []
 
-from os import path
+import os
 import sys
 import importlib
 from glob import glob
@@ -8,9 +8,43 @@ from glob import glob
 from .framework.bmi_bridge import bmi_factory
 
 
+def prepend_path(env, var, p, sep=os.pathsep):
+    try:
+        env[var] = sep.join([p, env[var]])
+    except KeyError:
+        env[var] = p
+
+    return p
+
+
+def set_env():
+    import os
+    import subprocess
+    import warnings
+
+    try:
+        prefix = subprocess.check_output(['csdms-config', '--var',
+                                          'PREFIX']).strip()
+    except subprocess.CalledProcessError:
+        warnings.warn('Unable to locate or run csdms-config program.')
+    else:
+        prepend_path(os.environ, 'SIDL_DLL_PATH',
+                     os.path.join(prefix, 'share', 'cca'), sep=';')
+
+    try:
+        libdir = subprocess.check_output(['cca-spec-babel-config', '--var',
+                                          'CCASPEC_BABEL_LIBS']).strip()
+    except subprocess.CalledProcessError:
+        warnings.warn('Unable to locate or run cca-spec-babel-config program.')
+    else:
+        prepend_path(os.environ, 'LD_LIBRARY_PATH', libdir)
+
+
+set_env()
+
 csdms_module = importlib.import_module('csdms')
-files = glob(path.join(csdms_module.__path__[0], '*so'))
-_COMPONENT_NAMES = (path.splitext(path.basename(f))[0] for f in files)
+files = glob(os.path.join(csdms_module.__path__[0], '*so'))
+_COMPONENT_NAMES = (os.path.splitext(os.path.basename(f))[0] for f in files)
 
 
 for name in _COMPONENT_NAMES:
