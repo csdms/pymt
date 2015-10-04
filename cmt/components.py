5504892fe28263916bd0604c3dfb38f83eb9b9c5
mcflugen <mcflugen@gmail.com>
2015-10-04 10:34:01 -0600

    Handle ImportError of csdms package.

--- cmt/components.py
+++ cmt/components.py
@@ -62,20 +62,28 @@ def setup_babel_environ():
         prepend_env_path('LD_LIBRARY_PATH', ccaspec_babel_libs)
 
 
-setup_babel_environ()
+def import_csdms_components():
+    setup_babel_environ()
 
-csdms_module = importlib.import_module('csdms')
-files = glob(os.path.join(csdms_module.__path__[0], '*so'))
-_COMPONENT_NAMES = (os.path.splitext(os.path.basename(f))[0] for f in files)
-
-
-for name in _COMPONENT_NAMES:
     try:
-        module = importlib.import_module('.'.join(['csdms', name]))
+        csdms_module = importlib.import_module('csdms')
     except ImportError:
-        pass
+        warnings.warn('Unable to import csdms. Not loading components.')
     else:
-        if name in module.__dict__:
-            setattr(sys.modules[__name__], name,
-                    bmi_factory(module.__dict__[name]))
-            __all__.append(name)
+        files = glob(os.path.join(csdms_module.__path__[0], '*so'))
+        _COMPONENT_NAMES = (
+            os.path.splitext(os.path.basename(f))[0] for f in files)
+
+        for name in _COMPONENT_NAMES:
+            try:
+                module = importlib.import_module('.'.join(['csdms', name]))
+            except ImportError:
+                pass
+            else:
+                if name in module.__dict__:
+                    setattr(sys.modules[__name__], name,
+                            bmi_factory(module.__dict__[name]))
+                    __all__.append(name)
+
+
+import_csdms_components()
