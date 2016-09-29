31d3e331187b4079382ff4b56b2f1ab172cff979
mcflugen <mcflugen@gmail.com>
2016-09-29 11:06:49 -0600

    Updated deploy and install scripts.

--- .travis/deploy_to_anaconda.sh
+++ .travis/deploy_to_anaconda.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+PACKAGE_NAME=$1
+
+PREFIX=$(python -c "import sys; print(sys.prefix)")
+ANACONDA=$(which anaconda)
+
+if [[ $ANACONDA_TOKEN == "" ]]; then
+  ANACONDA_UPLOAD="$ANACONDA upload"
+else
+  ANACONDA_UPLOAD="$ANACONDA -t $ANACONDA_TOKEN upload"
+fi
+
 if [[ "$TRAVIS_TAG" == v* ]]; then
   export CHANNEL="main"
 else
@@ -5,6 +18,7 @@ else
 fi
 
 echo "Uploading to $CHANNEL"
-anaconda -t $ANACONDA_TOKEN upload --force --user csdms --channel $CHANNEL $HOME/miniconda/conda-bld/**/pymt*bz2
+$ANACONDA_UPLOAD --force --user csdms --channel $CHANNEL \
+  $PREFIX/conda-bld/**/$PACKAGE_NAME*bz2
 
 echo "Done."
--- .travis/install_python.sh
+++ .travis/install_python.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
+PREFIX=$1
+if [[ $PREFIX == "" ]]; then
+  PREFIX=$HOME/miniconda
+fi
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    OS="MacOSX-x86_64";
+  OS="MacOSX-x86_64";
 else
-    OS="Linux-x86_64";
+  OS="Linux-x86_64";
 fi
 if [[ "$TRAVIS_PYTHON_VERSION" == 2.* ]]; then
-      wget http://repo.continuum.io/miniconda/Miniconda-latest-$OS.sh -O miniconda.sh;
+  wget http://repo.continuum.io/miniconda/Miniconda-latest-$OS.sh -O miniconda.sh;
 else
-      wget http://repo.continuum.io/miniconda/Miniconda3-latest-$OS.sh -O miniconda.sh;
+  wget http://repo.continuum.io/miniconda/Miniconda3-latest-$OS.sh -O miniconda.sh;
 fi
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $PREFIX
+export PATH="$PREFIX/bin:$PATH"
 hash -r
 conda config --set always_yes yes --set changeps1 no
-conda info -a
 conda install python=$TRAVIS_PYTHON_VERSION
 conda install -q conda-build
 conda install -q anaconda-client
