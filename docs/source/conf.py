681a0ba5af557f5c05e0f3411ba40d9176347a58
mcflugen <mcflugen@gmail.com>
2017-01-19 11:38:15 -0700

    Removed numpydoc from sys.path.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,6 @@ import sys, os
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)))
-sys.path.insert(0, os.path.abspath(os.path.join(os.pardir, 'sphinxext')))
 
 # -- General configuration -----------------------------------------------------
 
