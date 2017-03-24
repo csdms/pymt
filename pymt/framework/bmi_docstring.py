6c9c98861d18b7a502f93a7895ef394aa682f801
mcflugen <mcflugen@gmail.com>
2017-03-24 16:10:07 -0600

    Use jinja template for docstring.

--- pymt/framework/bmi_docstring.py
+++ pymt/framework/bmi_docstring.py
@@ -2,28 +2,34 @@
 import os
 import textwrap
 
+import jinja2
+
 from .bmi_metadata import load_bmi_metadata
 
 
 _DOCSTRING = u"""
-Basic Modeling Interface for {name}.
+Basic Model Interface for {name}.
 
-{desc}
+{{desc}}
 
-author: {author}
-version: {version}
-license: {license}
-DOI: {doi}
-URL: {url}
-
+author: {{author}}
+version: {{version}}
+license: {{license}}
+DOI: {{doi}}
+URL: {{url}}
+{% if parameters %}
 Parameters
 ----------
-{parameters}
+{% for param in parameters -%}
+{{param.name}} : {{param.type}}, optional
+    {{ "%s [default=%s %s]"|format(param.desc, param.value, param.units)|trim|wordwrap(70)|indent(4) }}
+{% endfor %}
+{% endif -%}
 
 Examples
 --------
->>> from pymt.components import {name}
->>> model = {name}()
+>>> from pymt.components import {{name}}
+>>> model = {{name}}()
 >>> (fname, initdir) = model.setup()
 >>> model.initialize(fname, dir=initdir)
 >>> for _ in xrange(10):
@@ -83,9 +89,14 @@ def bmi_docstring(name):
     meta = load_bmi_metadata(name)
     desc = '\n'.join(textwrap.wrap(meta['info'].summary))
     
-    return _DOCSTRING.format(
+    params = meta['defaults'].values()
+    params.sort(key=lambda p: p.name)
+
+    env = jinja2.Environment(loader=jinja2.DictLoader({'docstring': _DOCSTRING}))
+    return env.get_template('docstring').render(
         desc=desc, name=name,
-        parameters=build_parameters_section(meta['defaults']),
+        parameters=params,
+        # parameters=build_parameters_section(meta['defaults']),
         author=meta['info'].author,
         version=meta['info'].version,
         license=meta['info'].license,
