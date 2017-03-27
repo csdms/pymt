28e4dbc84b84f87cd9231a3c2e21db9f5f658d1e
mcflugen <mcflugen@gmail.com>
2017-03-27 11:42:35 -0600

    Remove old code.

--- pymt/framework/bmi_docstring.py
+++ pymt/framework/bmi_docstring.py
@@ -38,41 +38,6 @@ Examples
 """.strip()
 
 
-_PARAM_DECL = "{name} : {type}, optional"
-_PARAM_DESC = "{desc} [default={default} {units}]."
-
-
-def build_parameters_section(parameters):
-    """Build the paramters section of the docstring for a BMI model.
-
-    Parameters
-    ----------
-    parameters : dict
-        Parameters names and descriptions.
-
-    Returns
-    -------
-    str
-        The paramters section.
-    """
-    params = parameters.values()
-    params.sort(key=lambda p: p.name)
-
-    docstrings = []
-    for param in params:
-        decl = _PARAM_DECL.format(name=param.name, type=param.type)
-        desc = _PARAM_DESC.format(desc=param.desc, default=param.value,
-                                  units=param.units)
-
-        docstrings.extend(
-            textwrap.wrap(decl, subsequent_indent=' ' * (decl.find(':') + 3)))
-        docstrings.extend(
-            textwrap.wrap(desc, initial_indent=' ' * 4,
-                          subsequent_indent=' ' * 4))
-
-    return os.linesep.join(docstrings)
-
-
 def bmi_docstring(name):
     """Build the docstring for a BMI model.
 
@@ -96,7 +61,6 @@ def bmi_docstring(name):
     return env.get_template('docstring').render(
         desc=desc, name=name,
         parameters=params,
-        # parameters=build_parameters_section(meta['defaults']),
         author=meta['info'].author,
         version=meta['info'].version,
         license=meta['info'].license,
