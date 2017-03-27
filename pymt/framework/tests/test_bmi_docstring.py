bcb6d0a4fa75ce296b5c65b0fb5dc45e74045834
mcflugen <mcflugen@gmail.com>
2017-03-27 16:33:51 -0600

    Add "Cite as" section to docstring.

--- pymt/framework/tests/test_bmi_docstring.py
+++ pymt/framework/tests/test_bmi_docstring.py
@@ -11,6 +11,8 @@ Version: None
 License: None
 DOI: None
 URL: None
+
+
 Examples
 --------
 >>> from pymt.components import Model
@@ -43,6 +45,44 @@ Version: 0.1
 License: To kill
 DOI: 1977.12.23
 URL: welldoyapunk.org
+
+
+Examples
+--------
+>>> from pymt.components import DirtyHarry
+>>> model = DirtyHarry()
+>>> (fname, initdir) = model.setup()
+>>> model.initialize(fname, dir=initdir)
+>>> for _ in xrange(10):
+...     model.update()
+>>> model.finalize()
+""".strip())
+
+
+def test_cite_as():
+    docstring = bmi_docstring('DirtyHarry', author='Clint Eastwood',
+                              summary='The Good, The Bad, and the Ugly',
+                              version='0.1', license='To kill',
+                              doi='1977.12.23', url='welldoyapunk.org',
+                              cite_as=['ref1', 'ref2'])
+
+    assert_equal(docstring, """
+Basic Model Interface for DirtyHarry.
+
+The Good, The Bad, and the Ugly
+
+Author: Clint Eastwood
+Version: 0.1
+License: To kill
+DOI: 1977.12.23
+URL: welldoyapunk.org
+
+Cite as:
+
+    ref1
+
+    ref2
+
 Examples
 --------
 >>> from pymt.components import DirtyHarry
