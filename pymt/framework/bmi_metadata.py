f0acabccf6a5645d3a619b7bdeefd862dee8dffa
mcflugen <mcflugen@gmail.com>
2017-08-31 14:03:20 -0600

    Add find_model_metadata and PluginMetadata.

--- pymt/framework/bmi_metadata.py
+++ pymt/framework/bmi_metadata.py
@@ -1,14 +1,43 @@
 #! /usr/bin/env python
+"""Incorporate CSDMS Model Metadata into PyMT.
+
+PyMT uses the CSDMS model_metadata package, to
+incorporate CSDMS Model Metadata into
+BMI-enabled components when they are imported
+into PyMT.  This ensures that:
+
+*  Idenitying model information stays with the
+   component as it appears within the CSDMS
+   Modeling Framework. This ensures that the
+   original author of the model is given
+   appropriate credit and not forgotten in the
+   wrapping of their model. In addition,
+   a citation(s) is clearly displayed for the
+   model. This ensures that a user
+   running the model, either through PyMT or
+   otherwise, will properly cite the original
+   work that describes to model when publishing
+   results that use the model.
+*  Model input parameters are properly validated
+   and model input files are properly constructed
+   when preparing a model simulation.
+"""
+
 import os
+import sys
 import warnings
 import re
 import fnmatch
 from collections import namedtuple
 import types
+import pkg_resources
 
 import yaml
 
-from ..babel import query_config_var
+from model_metadata import ModelMetadata
+
+from ..babel import query_config_var, BabelConfigError
+
 
 
 Parameter = namedtuple('Parameter', ('name', 'value', 'units', 'desc', 'type'))
@@ -142,7 +171,10 @@ def bmi_data_dir(name):
     str
         The absolute path to the data directory for the component.
     """
-    datarootdir = query_config_var('datarootdir')
+    try:
+        datarootdir = query_config_var('datarootdir')
+    except BabelConfigError:
+        datarootdir = os.path.join(sys.prefix, 'share')
     return os.path.join(datarootdir, 'csdms', name)
 
 
@@ -301,3 +333,35 @@ def load_bmi_metadata(name):
     meta['parameters'] = parameters
 
     return meta
+
+
+def find_model_metadata(plugin):
+    """Find the path to a plugin's metadata.
+
+    Parameters
+    ----------
+    plugin : PyMT plugin
+        A PyMT plugin object.
+
+    Returns
+    -------
+    str
+        Path to the folder that contains the plugin's metadata.
+    """
+    try:
+        model_metadata_dir = plugin.METADATA
+    except AttributeError:
+        path_to_mmd = bmi_data_dir(plugin.__name__)
+    else:
+        path_to_mmd = pkg_resources.resource_filename(plugin.__module__,
+                                                      model_metadata_dir)
+
+    return path_to_mmd
+
+
+class PluginMetadata(ModelMetadata):
+
+    def __init__(self, model):
+        path_to_mmd = find_model_metadata(model)
+
+        ModelMetadata.__init__(self, path_to_mmd)
