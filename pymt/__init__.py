18813ca36853296e09a7a4c38cd931f5bb2f8810
Mark Piper <mark.piper@colorado.edu>
2018-02-20 14:24:40 -0700

    Use a try block for numpy<1.14

--- pymt/__init__.py
+++ pymt/__init__.py
@@ -4,4 +4,7 @@ del get_versions
 
 # See https://github.com/numpy/numpy/blob/master/doc/release/1.14.0-notes.rst#many-changes-to-array-printing-disableable-with-the-new-legacy-printing-mode
 import numpy as np
-np.set_printoptions(legacy='1.13')
+try:
+    np.set_printoptions(legacy='1.13')
+except TypeError:
+    pass
