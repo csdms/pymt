2b721eb9525394254b91f8ea73522551f6a0fd83
mcflugen <mcflugen@gmail.com>
2018-06-20 15:25:32 -0600

    Add __le__, __ge__ to Status class.

--- pymt/status.py
+++ pymt/status.py
@@ -1,14 +1,14 @@
 #! /bin/env python
-
 """
+>>> from __future__ import print_function
 >>> a = CREATED
->>> print a
+>>> print(a)
 Created
 >>> b = INITIALIZING
->>> print b
+>>> print(b)
 Initializing
 >>> c = INITIALIZED
->>> print c
+>>> print(c)
 Initialized
 >>> (a>b, a==b, a<b)
 (False, False, True)
@@ -16,37 +16,56 @@ Initialized
 (True, True, True)
 """
 
-class Status (object):
-    def __init__ (self, string, val):
+class Status(object):
+    def __init__(self, string, val):
         self.string = string
         self.val = val
-    def __str__ (self):
+    def __str__(self):
         return self.string
-    def __gt__ (self, other):
-        return self.val>other.val
-    def __eq__ (self, other):
-        return self.val==other.val
-    def __lt__ (self, other):
-        return self.val<other.val
+    def __gt__(self, other):
+        return self.val > other.val
+    def __ge__(self, other):
+        return self.val >= other.val
+    def __eq__(self, other):
+        return self.val == other.val
+    def __lt__(self, other):
+        return self.val < other.val
+    def __le__(self, other):
+        return self.val <= other.val
 
-class StatusConst (Status):
-    def __init__ (self):
+
+class StatusConst(Status):
+    def __init__(self):
         pass
 
-class StatusCreated (StatusConst):
+
+class StatusCreated(StatusConst):
     (string, val) = ('Created', 0)
-class StatusInitializing (StatusConst):
+
+
+class StatusInitializing(StatusConst):
     (string, val) = ('Initializing', 10)
-class StatusInitialized (StatusConst):
+
+
+class StatusInitialized(StatusConst):
     (string, val) = ('Initialized', 20)
-class StatusUpdating (StatusConst):
+
+
+class StatusUpdating(StatusConst):
     (string, val) = ('Updating', 30)
-class StatusUpdated (StatusConst):
+
+
+class StatusUpdated(StatusConst):
     (string, val) = ('Updated', 40)
-class StatusFinalizing (StatusConst):
+
+
+class StatusFinalizing(StatusConst):
     (string, val) = ('Finalizing', 50)
-class StatusFinalized (StatusConst):
+
+
+class StatusFinalized(StatusConst):
     (string, val) = ('Finalized', 60)
+
 
 CREATED = StatusCreated ()
 INITIALIZING = StatusInitializing ()
