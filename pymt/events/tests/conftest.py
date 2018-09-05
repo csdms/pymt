9c1f39389b5d553a88bc6266734a2109f9f771a0
mcflugen <mcflugen@gmail.com>
2018-09-05 16:33:39 -0600

    Add pytest fixture for air/earth ports.

---
+++ pymt/events/tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def with_earth_and_air():
+    from pymt.framework.services import (
+        register_component_classes,
+        instantiate_component,
+        del_services,
+    )
+
+    del_services()
+
+    register_component_classes(
+        ["pymt.testing.services.AirPort", "pymt.testing.services.EarthPort"]
+    )
+    instantiate_component("AirPort", "air_port")
+    instantiate_component("EarthPort", "earth_port")
