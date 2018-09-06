45a0fdad4dd5b95e7e6a33549ccf26a3b3a20347
mcflugen <mcflugen@gmail.com>
2018-09-05 21:05:11 -0600

    Fix comparisons with masked arrays and approx.

--- pymt/printers/nc/tests/test_database.py
+++ pymt/printers/nc/tests/test_database.py
@@ -7,20 +7,6 @@ from pytest import approx
 from pymt.grids import RasterField
 from pymt.printers.nc.database import Database
 
-_TMP_DIR = "tmp"
-
-
-def setup():
-    import tempfile
-
-    globals().update({"_TMP_DIR": tempfile.mkdtemp(dir=".")})
-
-
-def teardown():
-    from shutil import rmtree
-
-    rmtree(_TMP_DIR)
-
 
 def open_nc_file(filename):
     import netCDF4 as nc
@@ -28,12 +14,12 @@ def open_nc_file(filename):
     return nc.Dataset(filename, "r", format="NETCDF4")
 
 
-def test_2d_constant_shape():
+def test_2d_constant_shape(tmpdir):
     # Create field and add some data to it that we will write to a
     # NetCDF file database.
 
     # nc_file = 'Elevation_time_series_0000.nc'
-    nc_file = os.path.join(_TMP_DIR, "2d_elevation_time_series.nc")
+    nc_file = "2d_elevation_time_series.nc"
 
     data = np.arange(6.)
 
@@ -42,102 +28,102 @@ def test_2d_constant_shape():
 
     # Create database of 'Elevation' values. Data are written to the
     # NetCDF file Elevation_time_series.nc.
+    with tmpdir.as_cwd():
+        db = Database()
+        db.open(nc_file, "Elevation")
+        db.write(field)
 
-    db = Database()
-    db.open(nc_file, "Elevation")
-    db.write(field)
+        assert os.path.isfile(nc_file)
 
-    assert os.path.isfile(nc_file)
+        # Append data to the NetCDF file.
+        data *= 2.
+        db.write(field)
 
-    # Append data to the NetCDF file.
-    data *= 2.
-    db.write(field)
+        db.close()
 
-    db.close()
+        try:
+            root = open_nc_file(nc_file)
+        except Exception:
+            raise AssertionError("%s: Could not open" % nc_file)
+        else:
+            assert list(root.dimensions.keys()) == ["y", "x", "time"]
+            assert list(root.variables) == ["mesh", "y", "x", "Elevation", "time"]
 
-    try:
-        root = open_nc_file(nc_file)
-    except Exception:
-        raise AssertionError("%s: Could not open" % nc_file)
-    else:
-        assert ["y", "x", "time"] == list(root.dimensions.keys())
-        assert ["mesh", "y", "x", "Elevation", "time"] == list(root.variables)
+            assert len(root.dimensions["x"]) == 3
+            assert len(root.dimensions["y"]) == 2
+            assert len(root.dimensions["time"]) == 2
 
-        assert len(root.dimensions["x"]) == 3
-        assert len(root.dimensions["y"]) == 2
-        assert len(root.dimensions["time"]) == 2
+            assert root.variables["Elevation"].shape == (2, 2, 3)
 
-        assert root.variables["Elevation"].shape == (2, 2, 3)
+            assert root.variables["Elevation"][0].data == approx(
+                np.arange(6.).reshape(2, 3)
+            )
+            assert root.variables["Elevation"][1].data == approx(
+                np.arange(6.).reshape((2, 3)) * 2.
+            )
 
-        assert np.all(
-            root.variables["Elevation"][0] == approx(np.arange(6.).reshape(2, 3))
-        )
-        assert np.all(
-            root.variables["Elevation"][1] == approx(np.arange(6.).reshape((2, 3)) * 2.)
-        )
+            assert root.variables["y"] == approx([0., 1.])
+            assert root.variables["x"] == approx([0., 1., 2.])
 
-        assert root.variables["y"] == approx([0., 1.])
-        assert root.variables["x"] == approx([0., 1., 2.])
+            assert root.variables["Elevation"].long_name == "Elevation"
+            assert root.variables["Elevation"].units == "-"
 
-        assert root.variables["Elevation"].long_name == "Elevation"
-        assert root.variables["Elevation"].units == "-"
-
-        root.close()
+            root.close()
 
 
-def test_2d_changing_shape():
+def test_2d_changing_shape(tmpdir):
     # Create field and add some data to it that we will write to a
     # NetCDF file database.
 
-    nc_file = os.path.join(_TMP_DIR, "Temperature_time_series.nc")
+    nc_file = "Temperature_time_series.nc"
 
     data = np.arange(6.)
 
     field = RasterField((3, 2), (1., 1.), (0., 0.))
     field.add_field("Temperature", data, centering="point")
 
-    db = Database()
-    db.open(nc_file, "Temperature")
-    db.write(field)
+    with tmpdir.as_cwd():
+        db = Database()
+        db.open(nc_file, "Temperature")
+        db.write(field)
 
-    assert os.path.isfile(nc_file)
+        assert os.path.isfile(nc_file)
 
-    # Create a new field and write the data to the database. Since
-    # the size of the field has changed, the data will be written
-    # to a new file, Elevation_time_series_0000.nc.
+        # Create a new field and write the data to the database. Since
+        # the size of the field has changed, the data will be written
+        # to a new file, Elevation_time_series_0000.nc.
 
-    field = RasterField((3, 3), (1., 1.), (0., 0.))
-    data = np.arange(9.)
-    field.add_field("Temperature", data, centering="point")
+        field = RasterField((3, 3), (1., 1.), (0., 0.))
+        data = np.arange(9.)
+        field.add_field("Temperature", data, centering="point")
 
-    db.write(field)
-    assert os.path.isfile(os.path.join(_TMP_DIR, "Temperature_time_series_0000.nc"))
+        db.write(field)
+        assert os.path.isfile("Temperature_time_series_0000.nc")
 
-    db.close()
+        db.close()
 
-    try:
-        nc_file = os.path.join(_TMP_DIR, "Temperature_time_series_0000.nc")
-        root = open_nc_file(nc_file)
-    except Exception:
-        raise AssertionError("%s: Could not open" % nc_file)
-    else:
-        assert ["y", "x", "time"] == list(root.dimensions.keys())
-        assert ["mesh", "y", "x", "Temperature", "time"] == list(root.variables)
+        nc_file = "Temperature_time_series_0000.nc"
+        try:
+            root = open_nc_file(nc_file)
+        except Exception:
+            raise AssertionError("%s: Could not open" % nc_file)
+        else:
+            assert list(root.dimensions.keys()) == ["y", "x", "time"]
+            assert list(root.variables) == ["mesh", "y", "x", "Temperature", "time"]
 
-        assert len(root.dimensions["x"]) == 3
-        assert len(root.dimensions["y"]) == 3
-        assert len(root.dimensions["time"]) == 1
+            assert len(root.dimensions["x"]) == 3
+            assert len(root.dimensions["y"]) == 3
+            assert len(root.dimensions["time"]) == 1
 
-        assert root.variables["Temperature"].shape == (1, 3, 3)
+            assert root.variables["Temperature"].shape == (1, 3, 3)
+            assert root.variables["Temperature"][0].data == approx(
+                np.arange(9.).reshape((3, 3))
+            )
 
-        assert np.all(
-            root.variables["Temperature"][0] == approx(np.arange(9.).reshape((3, 3)))
-        )
+            assert root.variables["x"] == approx([0., 1., 2.])
+            assert root.variables["y"] == approx([0., 1., 2.])
 
-        assert root.variables["x"] == approx([0., 1., 2.])
-        assert root.variables["y"] == approx([0., 1., 2.])
+            assert root.variables["Temperature"].long_name == "Temperature"
+            assert root.variables["Temperature"].units == "-"
 
-        assert root.variables["Temperature"].long_name == "Temperature"
-        assert root.variables["Temperature"].units == "-"
-
-        root.close()
+            root.close()
