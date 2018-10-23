c743217f84fd48d3d4d3fcc80e0b0bafa051b0a4
mcflugen <mcflugen@gmail.com>
2018-10-23 17:24:47 -0600

    If plugin name is not defined, don't worry about it.

--- pymt/components.py
+++ pymt/components.py
@@ -8,4 +8,8 @@ for plugin in load_pymt_plugins():
     __all__.append(plugin.__name__)
     setattr(sys.modules[__name__], plugin.__name__, plugin)
 
-del sys, load_pymt_plugins, plugin
+try:
+    del plugin
+except NameError:
+    pass
+del sys, load_pymt_plugins
