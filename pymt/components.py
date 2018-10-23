a76c3dd06ae862a2633e1c0ead19590c00fca03a
mcflugen <mcflugen@gmail.com>
2018-10-23 16:52:02 -0600

    Load components/plugins with load_pymt_plugins.

--- pymt/components.py
+++ pymt/components.py
@@ -1,9 +1,11 @@
 __all__ = []
 
 import sys
-from .plugin import load_csdms_plugins
 
+from .plugin import load_pymt_plugins
 
-for plugin in load_csdms_plugins():
+for plugin in load_pymt_plugins():
     __all__.append(plugin.__name__)
     setattr(sys.modules[__name__], plugin.__name__, plugin)
+
+del sys, load_pymt_plugins, plugin
