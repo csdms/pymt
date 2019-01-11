8c8e255cdc5affa6d5f5551563b45b3a5d3d331d
mcflugen <mcflugen@gmail.com>
2019-01-11 14:11:05 -0700

    Mock modules used in pymt.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,34 @@
 #
 import os
 import sys
+
 # sys.path.insert(0, os.path.abspath('..'))
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = [
+    "numpy",
+    "scipy",
+    "matplotlib",
+    "xarray",
+    "shapely",
+    "netcdf4",
+    "yaml",
+    "esmpy",
+    "cfunits",
+    "model_metadata",
+    "scripting",
+    "landlab",
+]
+
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 import pymt
 
@@ -33,24 +60,31 @@ import pymt
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 # extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
-              'sphinx.ext.todo', 'sphinx.ext.coverage',
-              'sphinx.ext.mathjax', 'sphinx.ext.ifconfig',
-              'sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.autosummary']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+]
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'pymt'
+project = u"pymt"
 copyright = u"2018, Eric Hutton"
 author = u"Eric Hutton"
 
@@ -73,10 +107,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*tests*']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "*tests*"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -87,7 +121,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -98,13 +132,13 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pymtdoc'
+htmlhelp_basename = "pymtdoc"
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -113,15 +147,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -131,9 +162,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'pymt.tex',
-     u'pymt Documentation',
-     u'Eric Hutton', 'manual'),
+    (master_doc, "pymt.tex", u"pymt Documentation", u"Eric Hutton", "manual")
 ]
 
 
@@ -141,11 +170,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'pymt',
-     u'pymt Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "pymt", u"pymt Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------
@@ -154,16 +179,18 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'pymt',
-     u'pymt Documentation',
-     author,
-     'pymt',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "pymt",
+        u"pymt Documentation",
+        author,
+        "pymt",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
 
 napoleon_numpy_docstring = True
 napoleon_google_docstring = False
 napoleon_include_init_with_doc = True
 napoleon_include_special_with_doc = True
-
