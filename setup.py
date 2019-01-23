798808550be49e941d72044622ae920171611283
mcflugen <mcflugen@gmail.com>
2019-01-23 14:36:21 -0700

    Include cmt in installed packages.

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ import versioneer
 def read_requirements():
     import os
 
-
     path = os.path.dirname(os.path.abspath(__file__))
-    requirements_file = os.path.join(path, 'requirements.txt')
+    requirements_file = os.path.join(path, "requirements.txt")
     try:
-        with open(requirements_file, 'r') as req_fp:
+        with open(requirements_file, "r") as req_fp:
             requires = req_fp.read().split()
     except IOError:
         return []
@@ -18,18 +17,15 @@ def read_requirements():
         return [require.split() for require in requires]
 
 
-setup(name='PyMT',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description='The CSDMS Python Modeling Toolkit',
-      author='Eric Hutton',
-      author_email='huttone@colorado.edu',
-      url='http://csdms.colorado.edu',
-      setup_requires=['setuptools', ],
-      packages=find_packages(exclude=("tests*", "cmt")),
-      entry_points={
-          'console_scripts': [
-              'cmt-config=cmt.cmd.cmt_config:main',
-          ],
-      },
+setup(
+    name="pymt",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="The CSDMS Python Modeling Toolkit",
+    author="Eric Hutton",
+    author_email="huttone@colorado.edu",
+    url="http://csdms.colorado.edu",
+    setup_requires=["setuptools"],
+    packages=find_packages(exclude=("tests*",)),
+    entry_points={"console_scripts": ["cmt-config=cmt.cmd.cmt_config:main"]},
 )
