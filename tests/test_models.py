d9e1ed6ef8d147685ea3497c2d5e4f4d7e9a3810
mcflugen <mcflugen@gmail.com>
2019-02-07 22:22:05 -0700

    Skip the finalize tests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -31,6 +31,7 @@ def test_model_update(cls):
     assert model.get_current_time() > model.get_start_time()
 
 
+@pytest.mark.skip("don't test finalize")
 @pytest.mark.parametrize("cls", models.__all__)
 def test_model_finalize(cls):
     model = models.__dict__[cls]()
