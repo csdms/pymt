b3dd0c58cc59fc6382fb33f99b081c23193a252f
mcflugen <mcflugen@gmail.com>
2019-05-16 12:00:32 -0600

    chore: add install requirements

--- setup.py
+++ setup.py
@@ -3,29 +3,33 @@ from setuptools import setup, find_packages
 import versioneer
 
 
-def read_requirements():
-    import os
-
-    path = os.path.dirname(os.path.abspath(__file__))
-    requirements_file = os.path.join(path, "requirements.txt")
-    try:
-        with open(requirements_file, "r") as req_fp:
-            requires = req_fp.read().split()
-    except IOError:
-        return []
-    else:
-        return [require.split() for require in requires]
-
-
 setup(
-    name="pymt",
+    name="pymt-core",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="The CSDMS Python Modeling Toolkit",
+    long_description=open("README.rst").read(),
     author="Eric Hutton",
     author_email="huttone@colorado.edu",
     url="http://csdms.colorado.edu",
     setup_requires=["setuptools"],
+    install_requires=[
+        "cfunits",
+        "deprecated",
+        # "esmpy",
+        "jinja2",
+        "landlab",
+        "matplotlib",
+        "model_metadata",
+        "netcdf4",
+        "numpy",
+        "pyyaml",
+        "scipy",
+        "py-scripting",
+        "shapely",
+        "six",
+        "xarray",
+    ],
     packages=find_packages(exclude=("tests*",)),
     entry_points={"console_scripts": ["cmt-config=cmt.cmd.cmt_config:main"]},
 )
