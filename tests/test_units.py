987efea8cb5964b484b126df5700d2d505a1413f
mcflugen <mcflugen@gmail.com>
2020-08-01 22:51:24 -0600

    add unit tests for udunits2 module

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,51 +1,60 @@
 import random
 
 import numpy as np
-from pytest import approx, raises
+from pytest import approx, mark, raises
 
 from pymt.units import (
-    UnitConverter,
+    # UnitConverter,
     transform_azimuth_to_math,
     transform_math_to_azimuth,
 )
 
+from pymt.udunits2 import Unit, UnitConverter
+from pymt.errors import BadUnitError, IncompatibleUnitsError
+
 
 def test_unit_converter_length():
-    convert = UnitConverter("m")
-    assert convert(1.0, "km") == approx(1e-3)
+    meters = Unit("m")
+    assert meters.convert_to(1.0, "km") == approx(1e-3)
+
+    convert = UnitConverter("m", "km")
+    assert convert(1.0) == approx(1e-3)
 
 
 def test_unit_converter_time():
-    convert = UnitConverter("h")
-    assert convert(1.0, "s") == approx(3600.0)
+    convert = UnitConverter("h", "s")
+    assert convert(1.0) == approx(3600.0)
 
 
 def test_unit_converter_same_units():
-    convert = UnitConverter("h")
-    assert convert(1.0, "h") == approx(1.0)
+    convert = UnitConverter("h", "h")
+    assert convert(1.0) == approx(1.0)
 
 
-def test_unit_converter_bad_from_units():
-    with raises(ValueError):
-        UnitConverter("not-a-unit")
+@mark.parametrize(
+    ("to_", "from_"),
+    [("not_a_unit", "m"), ("m", "not_a_unit"), ("not_a_unit", "not_a_unit")],
+)
+def test_unit_converter_bad_from_units(to_, from_):
+    with raises(BadUnitError):
+        UnitConverter(to_, from_)
 
 
 def test_unit_converter_incompatible_units():
-    convert = UnitConverter("s")
-    with raises(ValueError):
-        convert(1.0, "m")
+    with raises(IncompatibleUnitsError):
+        convert = UnitConverter("s", "m")
 
 
 def test_unit_converter_inverse():
     val = random.random()
-    convert = UnitConverter("s")
-    assert convert.inv(convert(val, "h"), "h") == approx(val)
+    convert = UnitConverter("s", "h")
+    assert convert(convert(val), inverse=True) == approx(val)
 
 
 def test_unit_converter_units_attr():
-    convert = UnitConverter("m")
-    assert isinstance(convert.units, str)
-    assert convert.units == "m"
+    convert = UnitConverter("m", "km")
+    assert convert.src_units == "m"
+    assert convert.dst_units == "km"
 
 
 def test_math_to_azimuth():
