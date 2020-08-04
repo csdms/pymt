5bd863dba1ac6a68fd2c53099292222301cee804
mcflugen <mcflugen@gmail.com>
2020-08-04 17:05:04 -0600

    add suppress_output context manager

--- pymt/utils/utils.py
+++ pymt/utils/utils.py
@@ -5,6 +5,10 @@ from functools import partial
 import click
 
 
+out = partial(click.secho, bold=True, err=True)
+err = partial(click.secho, fg="red", err=True)
+
+
 @contextlib.contextmanager
 def as_cwd(path):
     prev_cwd = os.getcwd()
@@ -13,5 +17,20 @@ def as_cwd(path):
     os.chdir(prev_cwd)
 
 
-out = partial(click.secho, bold=True, err=True)
-err = partial(click.secho, fg="red", err=True)
+@contextlib.contextmanager
+def suppress_stdout():
+    null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+    # Save the actual stdout (1) and stderr (2) file descriptors.
+    save_fds = [os.dup(1), os.dup(2)]
+
+    os.dup2(null_fds[0], 1)
+    os.dup2(null_fds[1], 2)
+
+    yield
+
+    # Re-assign the real stdout/stderr back to (1) and (2)
+    os.dup2(save_fds[0], 1)
+    os.dup2(save_fds[1], 2)
+    # Close the null files
+    for fd in null_fds + save_fds:
+        os.close(fd)
