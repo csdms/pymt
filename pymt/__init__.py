6e929610b9039ddbc03440ab85733601406a7a79
mcflugen <mcflugen@gmail.com>
2020-08-04 20:21:28 -0600

    remove legacy numpy printing

--- pymt/__init__.py
+++ pymt/__init__.py
@@ -1,20 +1,6 @@
-import os
-import sys
-
-import numpy as np
-
 from ._version import get_versions
+from ._udunits2 import UnitSystem
+
 
 __version__ = get_versions()["version"]
 del get_versions
-
-# See https://github.com/numpy/numpy/blob/master/doc/release/1.14.0-notes.rst#many-changes-to-array-printing-disableable-with-the-new-legacy-printing-mode
-
-try:
-    np.set_printoptions(legacy="1.13")
-except TypeError:
-    pass
-del np
-
-
-del os, sys
