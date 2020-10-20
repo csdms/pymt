15e4b0db446df8a57e52449640e5cb6a206d57dd
mcflugen <mcflugen@gmail.com>
2020-10-20 11:49:35 -0600

    add MODELS to hold the loaded models

--- pymt/__init__.py
+++ pymt/__init__.py
@@ -1,8 +1,11 @@
 import pkg_resources
 
 from ._udunits2 import UnitSystem
+from .model_collection import ModelCollection
+
+MODELS = ModelCollection()
 
 __version__ = pkg_resources.get_distribution("pymt").version
-__all__ = ["UnitSystem"]
+__all__ = ["UnitSystem", "MODELS"]
 
 del pkg_resources
